464198eb738876ef535fb1d98c565540a14dc5bd
Benjamin Clark <clarkben@fb.com>
2019-05-23 18:07:30 -0700

    Deployment script that builds iD, preps the index.html file with our env-specific paths, and copies it to S3.
    Summary:
    WIP: Automate all the things!
    
    This is an attempt to make deploying iD a little easier! I presume we'll need to do this a bunch over the coming months.
    
    Test Plan:
    Install the AWS CLI and python3 on your system.
    Set up your aws credentials with 'aws configure'.
    cd to the iD dir, and then:
    
    python3 aws_deploy.py
    
    Reviewers: crk, gaoxm
    
    Reviewed By: gaoxm
    
    Subscribers: crk, gaoxm, saikatbasu
    
    Differential Revision: https://phabricator.intern.facebook.com/D15326737
    
    Tasks: T44340319
    
    Signature: 15326737:1558648472:18e577bb6eca76c21f75d9135fbe88353033281f

---
+++ aws_deploy.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import os
+import platform
+import re
+import subprocess
+
+"""Script to build & upload RapiD to mapwith.ai's S3 bucket
+Usage: python3 aws_deploy.py
+
+Assumptions: 
+1) You have the aws cli installed, and have run 'AWS configure' to set up your secret ID / key pair to stay authenticated. 
+
+2) You have installed python 3. 
+
+3) You are running from within the iD directory. 
+"""
+
+def preflight_build(): 
+    print("Calculating build hash and distdir...")
+    output = subprocess.run(["git",  "rev-parse",  "--short",  "HEAD"], capture_output=True)
+
+    hash = output.stdout.decode("utf-8").strip()
+    print("\ngithash: " + hash)
+
+    distdir = hash + "-dist"
+    print("\ndistdir: " + distdir)
+
+    print("\nRunning npm install")
+    subprocess.check_call(["npm", "install"])
+
+    print("\nRunning npm dist")
+    subprocess.check_call(["npm", "run", "dist"])
+    
+    print("\nCopying dist folder")
+    subprocess.check_call(["cp", "-a", "dist", distdir])
+
+    print("\nPrepping rapid.html with correct asset, css, and javascript paths")
+
+    index = "dist/index.html"
+    newindex = os.path.join(distdir, "index.html")
+
+    with open(index, 'r') as input:
+        with open(newindex, 'w+') as output:
+
+            for s in input:
+                s = s.replace("iD.css", f"/rapid/{distdir}/iD.css") \
+                .replace("iD.js", f"/rapid/{distdir}/iD.js") \
+                .replace(
+                    "var id = iD.Context();", 
+                    f"var id = iD.Context().assetPath('/rapid/{distdir}/');"
+                )
+                output.write(s);
+
+    print("\nCopying new " + distdir + "folder and index file to s3.")
+    subprocess.check_call(("aws s3 cp " + distdir + " s3://world.ai.rapid/rapid/" + distdir + " --recursive").split())
+    subprocess.check_call(("aws s3 cp " + newindex + " s3://world.ai.rapid/rapid/" + hash + "-rapid.html").split())
+
+if __name__ == "__main__":
+    preflight_build()
