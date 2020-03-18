fc894fd59bc8e0f2ff0da372c9927fe121b8e4c3
Benjamin Clark <clarkben@fb.com>
2020-03-18 00:01:30 -0400

    Substantially reduce the control surface that we present to users- fewer tools on the toolbar, shorter Help text, many sidebar controls are reduced or outright hidden.

--- modules/ui/help.js
+++ modules/ui/help.js
@@ -34,9 +34,9 @@ export function uiHelp(context) {
             'navigation_h',
             'navigation_drag',
             'navigation_zoom',
-            'features_h',
-            'features',
-            'nodes_ways'
+            // 'features_h',
+            // 'features',
+            // 'nodes_ways'
         ]],
         ['editing', [
             'select_h',
@@ -47,30 +47,25 @@ export function uiHelp(context) {
             'multiselect_lasso',
             'undo_redo_h',
             'undo_redo',
-            'save_h',
-            'save',
-            'save_validation',
-            'upload_h',
-            'upload',
-            'backups_h',
-            'backups',
+            'export_h',
+            'export',
             'keyboard_h',
             'keyboard'
         ]],
-        ['feature_editor', [
-            'intro',
-            'definitions',
-            'type_h',
-            'type',
-            'type_picker',
-            'fields_h',
-            'fields_all_fields',
-            'fields_example',
-            'fields_add_field',
-            'tags_h',
-            'tags_all_tags',
-            'tags_resources'
-        ]],
+        // ['feature_editor', [
+        //     'intro',
+        //     'definitions',
+        //     'type_h',
+        //     'type',
+        //     'type_picker',
+        //     'fields_h',
+        //     'fields_all_fields',
+        //     'fields_example',
+        //     'fields_add_field',
+        //     'tags_h',
+        //     'tags_all_tags',
+        //     'tags_resources'
+        // ]],
         ['points', [
             'intro',
             'add_point_h',
@@ -82,80 +77,80 @@ export function uiHelp(context) {
             'delete_point',
             'delete_point_command'
         ]],
-        ['lines', [
-            'intro',
-            'add_line_h',
-            'add_line',
-            'add_line_draw',
-            'add_line_finish',
-            'modify_line_h',
-            'modify_line_dragnode',
-            'modify_line_addnode',
-            'connect_line_h',
-            'connect_line',
-            'connect_line_display',
-            'connect_line_drag',
-            'connect_line_tag',
-            'disconnect_line_h',
-            'disconnect_line_command',
-            'move_line_h',
-            'move_line_command',
-            'move_line_connected',
-            'delete_line_h',
-            'delete_line',
-            'delete_line_command'
-        ]],
-        ['areas', [
-            'intro',
-            'point_or_area_h',
-            'point_or_area',
-            'add_area_h',
-            'add_area_command',
-            'add_area_draw',
-            'add_area_finish',
-            'square_area_h',
-            'square_area_command',
-            'modify_area_h',
-            'modify_area_dragnode',
-            'modify_area_addnode',
-            'delete_area_h',
-            'delete_area',
-            'delete_area_command'
-        ]],
-        ['relations', [
-            'intro',
-            'edit_relation_h',
-            'edit_relation',
-            'edit_relation_add',
-            'edit_relation_delete',
-            'maintain_relation_h',
-            'maintain_relation',
-            'relation_types_h',
-            'multipolygon_h',
-            'multipolygon',
-            'multipolygon_create',
-            'multipolygon_merge',
-            'turn_restriction_h',
-            'turn_restriction',
-            'turn_restriction_field',
-            'turn_restriction_editing',
-            'route_h',
-            'route',
-            'route_add',
-            'boundary_h',
-            'boundary',
-            'boundary_add'
-        ]],
-        ['notes', [
-            'intro',
-            'add_note_h',
-            'add_note',
-            'move_note',
-            'update_note_h',
-            'update_note',
-            'save_note_h',
-            'save_note'
-        ]],
+        // ['lines', [
+        //     'intro',
+        //     'add_line_h',
+        //     'add_line',
+        //     'add_line_draw',
+        //     'add_line_finish',
+        //     'modify_line_h',
+        //     'modify_line_dragnode',
+        //     'modify_line_addnode',
+        //     'connect_line_h',
+        //     'connect_line',
+        //     'connect_line_display',
+        //     'connect_line_drag',
+        //     'connect_line_tag',
+        //     'disconnect_line_h',
+        //     'disconnect_line_command',
+        //     'move_line_h',
+        //     'move_line_command',
+        //     'move_line_connected',
+        //     'delete_line_h',
+        //     'delete_line',
+        //     'delete_line_command'
+        // ]],
+        // ['areas', [
+        //     'intro',
+        //     'point_or_area_h',
+        //     'point_or_area',
+        //     'add_area_h',
+        //     'add_area_command',
+        //     'add_area_draw',
+        //     'add_area_finish',
+        //     'square_area_h',
+        //     'square_area_command',
+        //     'modify_area_h',
+        //     'modify_area_dragnode',
+        //     'modify_area_addnode',
+        //     'delete_area_h',
+        //     'delete_area',
+        //     'delete_area_command'
+        // ]],
+        // ['relations', [
+        //     'intro',
+        //     'edit_relation_h',
+        //     'edit_relation',
+        //     'edit_relation_add',
+        //     'edit_relation_delete',
+        //     'maintain_relation_h',
+        //     'maintain_relation',
+        //     'relation_types_h',
+        //     'multipolygon_h',
+        //     'multipolygon',
+        //     'multipolygon_create',
+        //     'multipolygon_merge',
+        //     'turn_restriction_h',
+        //     'turn_restriction',
+        //     'turn_restriction_field',
+        //     'turn_restriction_editing',
+        //     'route_h',
+        //     'route',
+        //     'route_add',
+        //     'boundary_h',
+        //     'boundary',
+        //     'boundary_add'
+        // ]],
+        // ['notes', [
+        //     'intro',
+        //     'add_note_h',
+        //     'add_note',
+        //     'move_note',
+        //     'update_note_h',
+        //     'update_note',
+        //     'save_note_h',
+        //     'save_note'
+        // ]],
 
         ['imagery', [
             'intro',
@@ -166,28 +161,28 @@ export function uiHelp(context) {
             'offset',
             'offset_change'
         ]],
-        ['streetlevel', [
-            'intro',
-            'using_h',
-            'using',
-            'photos',
-            'viewer'
-        ]],
-        ['gps', [
-            'intro',
-            'survey',
-            'using_h',
-            'using',
-            'tracing',
-            'upload'
-        ]],
-        ['qa', [
-            'intro',
-            'tools_h',
-            'tools',
-            'issues_h',
-            'issues'
-        ]]
+        // ['streetlevel', [
+        //     'intro',
+        //     'using_h',
+        //     'using',
+        //     'photos',
+        //     'viewer'
+        // ]],
+        // ['gps', [
+        //     'intro',
+        //     'survey',
+        //     'using_h',
+        //     'using',
+        //     'tracing',
+        //     'upload'
+        // ]],
+        // ['qa', [
+        //     'intro',
+        //     'tools_h',
+        //     'tools',
+        //     'issues_h',
+        //     'issues'
+        // ]]
     ];
 
     var headings = {
@@ -199,9 +194,9 @@ export function uiHelp(context) {
         'help.editing.select_h': 3,
         'help.editing.multiselect_h': 3,
         'help.editing.undo_redo_h': 3,
-        'help.editing.save_h': 3,
-        'help.editing.upload_h': 3,
-        'help.editing.backups_h': 3,
+        'help.editing.export_h': 3,
+        // 'help.editing.upload_h': 3,
+        // 'help.editing.backups_h': 3,
         'help.editing.keyboard_h': 3,
         'help.feature_editor.type_h': 3,
         'help.feature_editor.fields_h': 3,
@@ -210,32 +205,32 @@ export function uiHelp(context) {
         'help.points.move_point_h': 3,
         'help.points.delete_point_h': 3,
         'help.lines.add_line_h': 3,
-        'help.lines.modify_line_h': 3,
-        'help.lines.connect_line_h': 3,
-        'help.lines.disconnect_line_h': 3,
-        'help.lines.move_line_h': 3,
-        'help.lines.delete_line_h': 3,
-        'help.areas.point_or_area_h': 3,
-        'help.areas.add_area_h': 3,
-        'help.areas.square_area_h': 3,
-        'help.areas.modify_area_h': 3,
-        'help.areas.delete_area_h': 3,
-        'help.relations.edit_relation_h': 3,
-        'help.relations.maintain_relation_h': 3,
-        'help.relations.relation_types_h': 2,
-        'help.relations.multipolygon_h': 3,
-        'help.relations.turn_restriction_h': 3,
-        'help.relations.route_h': 3,
-        'help.relations.boundary_h': 3,
-        'help.notes.add_note_h': 3,
-        'help.notes.update_note_h': 3,
-        'help.notes.save_note_h': 3,
+        // 'help.lines.modify_line_h': 3,
+        // 'help.lines.connect_line_h': 3,
+        // 'help.lines.disconnect_line_h': 3,
+        // 'help.lines.move_line_h': 3,
+        // 'help.lines.delete_line_h': 3,
+        // 'help.areas.point_or_area_h': 3,
+        // 'help.areas.add_area_h': 3,
+        // 'help.areas.square_area_h': 3,
+        // 'help.areas.modify_area_h': 3,
+        // 'help.areas.delete_area_h': 3,
+        // 'help.relations.edit_relation_h': 3,
+        // 'help.relations.maintain_relation_h': 3,
+        // 'help.relations.relation_types_h': 2,
+        // 'help.relations.multipolygon_h': 3,
+        // 'help.relations.turn_restriction_h': 3,
+        // 'help.relations.route_h': 3,
+        // 'help.relations.boundary_h': 3,
+        // 'help.notes.add_note_h': 3,
+        // 'help.notes.update_note_h': 3,
+        // 'help.notes.save_note_h': 3,
         'help.imagery.sources_h': 3,
         'help.imagery.offsets_h': 3,
-        'help.streetlevel.using_h': 3,
-        'help.gps.using_h': 3,
-        'help.qa.tools_h': 3,
-        'help.qa.issues_h': 3
+        // 'help.streetlevel.using_h': 3,
+        // 'help.gps.using_h': 3,
+        // 'help.qa.tools_h': 3,
+        // 'help.qa.issues_h': 3
     };
 
     var replacements = {
@@ -423,21 +418,21 @@ export function uiHelp(context) {
             .append('div')
             .text(t('shortcuts.title'));
 
-        var walkthrough = toc
-            .append('li')
-            .attr('class', 'walkthrough')
-            .append('a')
-            .on('click', clickWalkthrough);
+        // var walkthrough = toc
+        //     .append('li')
+        //     .attr('class', 'walkthrough')
+        //     .append('a')
+        //     .on('click', clickWalkthrough);
 
-        walkthrough
-            .append('svg')
-            .attr('class', 'logo logo-walkthrough')
-            .append('use')
-            .attr('xlink:href', '#iD-logo-walkthrough');
+        // walkthrough
+        //     .append('svg')
+        //     .attr('class', 'logo logo-walkthrough')
+        //     .append('use')
+        //     .attr('xlink:href', '#iD-logo-walkthrough');
 
-        walkthrough
-            .append('div')
-            .text(t('splash.walkthrough'));
+        // walkthrough
+        //     .append('div')
+        //     .text(t('splash.walkthrough'));
 
 
         var helpContent = content
