1e7276dc21d4039e5cae6762497c0dd053709209
Bryan Housel <bryan@mapbox.com>
2020-04-24 10:27:54 -0400

    Disable gazer (it is watching too many files)

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const colors = require('colors/safe');
-const gaze = require('gaze');
+// const gaze = require('gaze');
 const StaticServer = require('static-server');
 
 const buildAll = require('./build');
@@ -8,20 +8,20 @@ const buildData = require('./build_data');
 const buildSrc = require('./build_src');
 const buildCSS = require('./build_css');
 
-const CSSFILES = ['css/**/*.css'];
-const SRCFILES = ['modules/**/*.js'];
-const DATAFILES = [
-  'data/**/*.{js,json}',
-  'data/core.yaml',
-  // ignore the output files of `buildData`
-  '!data/presets/categories.json',
-  '!data/presets/fields.json',
-  '!data/presets/presets.json',
-  '!data/presets.yaml',
-  '!data/taginfo.json',
-  '!data/territory-languages.json',
-  '!dist/locales/en.json'
-];
+// const CSSFILES = ['css/**/*.css'];
+// const SRCFILES = ['modules/**/*.js'];
+// const DATAFILES = [
+//   'data/**/*.{js,json}',
+//   'data/core.yaml',
+//   // ignore the output files of `buildData`
+//   '!data/presets/categories.json',
+//   '!data/presets/fields.json',
+//   '!data/presets/presets.json',
+//   '!data/presets.yaml',
+//   '!data/taginfo.json',
+//   '!data/territory-languages.json',
+//   '!dist/locales/en.json'
+// ];
 
 
 buildAll()
@@ -29,20 +29,20 @@ buildAll()
 
 
 function startServer() {
-  gaze(CSSFILES, (err, watcher) => {
-    watcher.on('all', () => buildCSS());
-  });
+  // gaze(CSSFILES, (err, watcher) => {
+  //   watcher.on('all', () => buildCSS());
+  // });
 
-  gaze(DATAFILES, (err, watcher) => {
-    watcher.on('all', () => {
-      buildData()
-        .then(() => buildSrc());
-    });
-  });
+  // gaze(DATAFILES, (err, watcher) => {
+  //   watcher.on('all', () => {
+  //     buildData()
+  //       .then(() => buildSrc());
+  //   });
+  // });
 
-  gaze(SRCFILES, (err, watcher) => {
-    watcher.on('all', () => buildSrc());
-  });
+  // gaze(SRCFILES, (err, watcher) => {
+  //   watcher.on('all', () => buildSrc());
+  // });
 
   const server = new StaticServer({ rootPath: __dirname, port: 8080, followSymlink: true });
   server.start(() => {
