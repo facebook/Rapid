f415cce02e6ab221c4e56042f280bfc0ed2d159a
Bryan Housel <bhousel@gmail.com>
2021-08-04 11:15:20 -0400

    Make scripts for all esbuild options, remove uglify Previously uglify was just minifying the legacy build, now esbuild can minify modern JS Also removed a few vestiges of rollup.

--- config/esbuild.config.legacy-min.js
+++ config/esbuild.config.legacy-min.js
@@ -3,12 +3,13 @@ import babel from 'esbuild-plugin-babel';
 
 esbuild
   .build({
+    minify: true,
     bundle: true,
     sourcemap: true,
     entryPoints: ['./modules/id.js'],
     legalComments: 'none',
     logLevel: 'info',
-    outfile: 'dist/iD.legacy.js',
+    outfile: 'dist/iD.legacy.min.js',
     target: 'es5',
     plugins: [babel({
       filter: /.*/,
@@ -20,3 +21,4 @@ esbuild
     })],
   })
   .catch(() => process.exit(1));
+
