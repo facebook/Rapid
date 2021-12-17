26fa00e40d2fd09658fb7186132844163d67fc69
Bryan Housel <bhousel@gmail.com>
2021-12-17 15:34:45 -0500

    Add some simple area styles

--- modules/pixi/pixiAreas.js
+++ modules/pixi/pixiAreas.js
@@ -37,9 +37,12 @@ export function pixiAreas(context) {
           graphics.name = entity.id;
           pixi.stage.addChild(graphics);
 
+          const style = styleMatch(entity.tags);
+
           datum = {
             coords: coords,
-            graphics: graphics
+            graphics: graphics,
+            style: style
           };
 
           _cache.set(entity.id, datum);
@@ -49,8 +52,8 @@ export function pixiAreas(context) {
         const path = utilArrayFlatten(datum.coords.map(coord => context.projection(coord)));
         datum.graphics
           .clear()
-          .lineStyle({ color: 0xff00ff, width: 3 })
-          .beginFill(0xff00ff, 0.4)
+          .lineStyle(datum.style.width, datum.style.color)
+          .beginFill(datum.style.color, datum.style.alpha)
           .drawPolygon(path)
           .endFill();
 
@@ -59,4 +62,187 @@ export function pixiAreas(context) {
   }
 
   return renderAreas;
+}
+
+
+const STYLES = {
+  red: {
+    width: 2,
+    color: 0xe06e5f,  // rgba(224, 110, 95)
+    alpha: 0.3
+  },
+  green: {
+    width: 2,
+    color: 0x8cd05f,  // rgb(140, 208, 95)
+    alpha: 0.3
+  },
+  blue: {
+    width: 2,
+    color: 0x77d4de,  // rgb(119, 211, 222)
+    alpha: 0.3
+  },
+  yellow: {
+    width: 2,
+    color: 0xffff94,  // rgb(255, 255, 148)
+    alpha: 0.25
+  },
+  gold: {
+    width: 2,
+    color: 0xc4be19,  // rgb(196, 189, 25)
+    alpha: 0.3
+  },
+  orange: {
+    width: 2,
+    color: 0xd6881a,  // rgb(214, 136, 26)
+    alpha: 0.3
+  },
+  pink: {
+    width: 2,
+    color: 0xe3a4f5,  // rgb(228, 164, 245)
+    alpha: 0.3
+  },
+  teal: {
+    width: 2,
+    color: 0x99e1aa,  // rgb(153, 225, 170)
+    alpha: 0.3
+  },
+  lightgreen: {
+    width: 2,
+    color: 0xbee83f,  // rgb(191, 232, 63)
+    alpha: 0.3
+  },
+  tan: {
+    width: 2,
+    color: 0xf5dcba,  // rgb(245, 220, 186)
+    alpha: 0.3
+  },
+  darkgray: {
+    width: 2,
+    color: 0x8c8c8c,  // rgb(140, 140, 140)
+    alpha: 0.5
+  },
+  lightgray: {
+    width: 2,
+    color: 0xaaaaaa,  // rgb(170, 170, 170)
+    alpha: 0.3
+  }
+};
+
+const TAGSTYLES = {
+  amenity: {
+    fountain: 'blue',
+    childcare: 'yellow',
+    kindergarten: 'yellow',
+    school: 'yellow',
+    college: 'yellow',
+    university: 'yellow',
+    research_institute: 'yellow',
+    parking: 'darkgray'
+  },
+  building: {
+    '*': 'red'
+  },
+  construction: {
+    '*': 'gold'
+  },
+  barrier: {
+    hedge: 'green'
+  },
+  golf: {
+    'green': 'lightgreen'
+  },
+  landuse: {
+    flowerbed: 'green',
+    forest: 'green',
+    grass: 'green',
+    recreation_ground: 'green',
+    village_green: 'green',
+    residential: 'gold',
+    retail: 'orange',
+    commercial: 'orange',
+    landfill: 'orange',
+    military: 'orange',
+    industrial: 'pink',
+    cemetery: 'lightgreen',
+    farmland: 'lightgreen',
+    meadow: 'lightgreen',
+    orchard: 'lightgreen',
+    vineyard: 'lightgreen',
+    farmyard: 'tan',
+    railway: 'darkgray',
+    quarry: 'darkgray'
+  },
+  leisure: {
+    garden: 'green',
+    golf_course: 'green',
+    nature_reserve: 'green',
+    park: 'green',
+    pitch: 'green',
+    track: 'yellow',
+    swimming_pool: 'blue'
+  },
+  man_made: {
+    adit: 'darkgray',
+    groyne: 'darkgray',
+    breakwater: 'darkgray'
+  },
+  military: {
+    '*': 'orange'
+  },
+  natural: {
+    bay: 'blue',
+    water: 'blue',
+    beach: 'yellow',
+    sand: 'yellow',
+    scrub: 'yellow',
+    wetland: 'teal',
+    bare_rock: 'darkgray',
+    cave_entrance: 'darkgray',
+    cliff: 'darkgray',
+    rock: 'darkgray',
+    scree: 'darkgray',
+    stone: 'darkgray',
+    shingle: 'darkgray',
+    glacier: 'lightgray',
+    '*': 'green'
+  },
+  power: {
+    'plant': 'pink'
+  },
+  sport: {
+    beachvolleyball: 'yellow',
+    baseball: 'yellow',
+    softball: 'yellow',
+    basketball: 'darkgray',
+    skateboard: 'darkgray'
+  },
+  waterway: {
+    dam: 'darkgray',
+    weir: 'darkgray'
+  }
+};
+
+
+function styleMatch(tags) {
+  let style = STYLES.lightgray;  // default
+  let selectivity = 999;
+
+  for (const k in tags) {
+    const v = tags[k];
+    const group = TAGSTYLES[k];
+    if (!group || !v) continue;
+
+    // smaller groups are more selective
+    let groupsize = Object.keys(group).length;
+    let stylename = group[v];
+    if (!stylename) stylename = group['*'];  // fallback value
+
+    if (stylename && groupsize < selectivity) {
+      style = STYLES[stylename];
+      selectivity = groupsize;
+      if (selectivity === 1) break;  // no need to keep looking at tags
+    }
+  }
+
+  return style;
 }
