36ba800e857146d3a75661ff6bd934c772973209
Benjamin Clark <clarkben@fb.com>
2022-12-14 17:30:53 -0500

    Fix #720 by fixing the waypoint iterator to consider polygons as well as lines.

--- modules/pixi/PixiLayerOsm.js
+++ modules/pixi/PixiLayerOsm.js
@@ -657,12 +657,14 @@ export class PixiLayerOsm extends AbstractLayer {
    * @param  projection   Pixi projection to use for rendering
    * @param  zoom         Effective zoom to use for rendering
    * @param  data         Visible OSM data to render, sorted by type
-   * @param  realated     Collections of related OSM IDs
+   * @param  related     Collections of related OSM IDs
    */
   renderMidpoints(frame, projection, zoom, data, related) {
     const MIN_MIDPOINT_DIST = 40;   // distance in pixels
-    const entities = data.lines;
     const graph = this.context.graph();
+
+    //Need to consider both lines and polygons for drawing our midpoints
+    const entities = new Map([...data.lines, ...data.polygons]);
 
     // Midpoints should be drawn above everything
     const mapUIContainer = this.scene.layers.get('map-ui').container;
@@ -671,7 +673,6 @@ export class PixiLayerOsm extends AbstractLayer {
     // Generate midpoints from all the highlighted ways
     let midpoints = new Map();
     const MIDPOINT_STYLE = { markerName: 'midpoint' };
-
     for (const [wayID, way] of entities) {
       // Include only ways that are selected, or descended from a relation that is selected
       if (!related.descendantIDs.has(wayID)) continue;
