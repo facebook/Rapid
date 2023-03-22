d3a48645a43134d154877c3eb65998fa4d4ada94
Bryan Housel <bhousel@gmail.com>
2023-03-22 14:48:47 -0400

    Don't need to generate a metafile for the legacy build
    This metafile is just used to produce the `npm run build:stats` esbuild
    visualizer output.. It won't be any different between modern and legacy builds.

--- config/esbuild.config.legacy.js
+++ config/esbuild.config.legacy.js
@@ -1,5 +1,4 @@
 import esbuild from 'esbuild';
-import fs from 'node:fs';
 
 esbuild
   .build({
@@ -8,12 +7,8 @@ esbuild
     entryPoints: ['./modules/main.js'],
     legalComments: 'none',
     logLevel: 'info',
-    metafile: true,
     outfile: 'dist/rapid.legacy.js',
     loader: { '.js': 'jsx' },
     target: 'es2016',
-  })
-  .then(result => {
-    fs.writeFileSync('./dist/esbuild-legacy.json', JSON.stringify(result.metafile, null, 2));
   })
   .catch(() => process.exit(1));
