b4b0693874950cabf38e9f2faf466c3553a67b12
Tanner Wuster <wuster@meta.com>
2023-03-23 12:11:10 -0600

    npm install was failing on build

--- modules/ui/map_in_map.js
+++ modules/ui/map_in_map.js
@@ -1,6 +1,6 @@
-import { geoScaleToZoom, geoZoomToScale, Projection, vecScale, vecSubtract } from '@id-sdk/math';
 import { select as d3_select } from 'd3-selection';
 import { zoom as d3_zoom, zoomIdentity as d3_zoomIdentity } from 'd3-zoom';
+import { Projection, geoScaleToZoom, geoZoomToScale, vecScale, vecSubtract } from '@rapid-sdk/math';
 import * as PIXI from 'pixi.js';
 
 import { t } from '../core/localizer';
