324d3b0db7b936c69421365ec572c007071b5c25
Bryan Housel <bhousel@gmail.com>
2023-04-20 10:11:12 -0400

    WIP on packing svg icons into the Pixi texture atlas (re: #925)

--- modules/ui/defs.js
+++ modules/ui/defs.js
@@ -1,6 +1,7 @@
 import { svg as d3_svg } from 'd3-fetch';
 import { select as d3_select } from 'd3-selection';
 
+
 /*
  * A standalone SVG `defs` element that contains the icon spritesheets for the user interface
  */
@@ -19,21 +20,64 @@ export function uiDefs(context) {
       .append('g')
       .attr('class', d => `spritesheet spritesheet-${d}`)
       .each((d, i, nodes) => {
+        const group = d3_select(nodes[i]);
         const url = context.asset(`img/${d}.svg`);
-        const group = nodes[i];
 
         d3_svg(url)
-          .then(svg => {
-            group.appendChild(
-              d3_select(svg.documentElement).attr('id', d).node()
-            );
-            if (d !== 'rapid-sprite') {   // allow icon fill colors to be overridden..
-              d3_select(group).selectAll('path')
-                .attr('fill', 'currentColor');
-            }
-          })
+          .then(svg => group.call(loadIcons, d, svg))
           .catch(e => console.error(e));  // eslint-disable-line
       });
+  }
+
+
+  function loadIcons(selection, spritesheetID, svg) {
+    const group = selection.node();
+    const element = svg.documentElement;
+
+    element.setAttribute('id', spritesheetID);
+    group.appendChild(element);
+
+    // Allow icon fill colors to be overridden..
+    if (spritesheetID !== 'rapid-sprite') {
+      selection.selectAll('path')
+        .attr('fill', 'currentColor');
+    }
+
+// WIP on #925
+//    // Pack icons into Pixi texture atlas
+//    selection.selectAll('symbol')
+//      .each((d, i, nodes) => {
+//        if (spritesheetID !== 'community-sprite') return;
+//
+//        const symbol = nodes[i];
+//        const iconID = symbol.getAttribute('id');
+//        const viewBox = symbol.getAttribute('viewBox');
+//
+//        // Make a new <svg> container
+//        let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+//        svg.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink');
+//        svg.setAttribute('width', '32');
+//        svg.setAttribute('height', '32');
+//        svg.setAttribute('color', '#fff');   // white so we can tint them
+//        svg.setAttribute('viewBox', viewBox);
+//
+//        // Clone children (this is essentially what <use> does)
+//        for (const child of symbol.childNodes) {
+//          svg.appendChild(child.cloneNode(true));  // true = deep clone
+//        }
+//
+//        const svgStr = (new XMLSerializer()).serializeToString(svg);
+//        svg = null;
+//
+//        const image = new Image();
+//        image.src = `data:image/svg+xml,${encodeURIComponent(svgStr)}`;
+//        image.onload = () => {
+//          // something like this
+//          const PADDING = 0;
+//          const texture = this._atlasAllocator.allocate(w, h, PADDING, image);
+//          this._textures.set(iconID, texture);
+//        };
+//      });
   }
 
   return render;
