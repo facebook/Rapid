ac89734caf8a0fe942219ccbfbaa1b49186cc95d
Benjamin Clark <clarkben@fb.com>
2023-05-12 14:36:11 -0400

    Add the env printing back.

--- scripts/aws_deploy.py
+++ scripts/aws_deploy.py
@@ -28,7 +28,7 @@ def deploy():
     print("\ngithash: " + hash)
 
     #print all env vars
-    #print( '\n'.join([f'{k}: {v}' for k, v in sorted(os.environ.items())]) )
+    print( '\n'.join([f'{k}: {v}' for k, v in sorted(os.environ.items())]) )
 
     identifier = os.environ["IDENTIFIER"]
     distdir = os.environ["DISTDIR"]
