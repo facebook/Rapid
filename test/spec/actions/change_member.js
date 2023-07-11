bfe880360cbc5e0260d08150e89254fa850b71a8
Giselle Santos <gisellesf@dcomp.ufs.br>
2023-07-11 20:12:02 -0300

    001-remover pasta test com Karma

--- test/spec/actions/change_member.js
+++
@@ -1,9 +0,0 @@
-describe('actionChangeMember', function () {
-    it('updates the member at the specified index', function () {
-        var node     = Rapid.osmNode(),
-            relation = Rapid.osmRelation({members: [{id: node.id}]}),
-            action   = Rapid.actionChangeMember(relation.id, {id: node.id, role: 'node'}, 0),
-            graph    = action(new Rapid.Graph([node, relation]));
-        expect(graph.entity(relation.id).members).to.eql([{id: node.id, role: 'node'}]);
-    });
-});
