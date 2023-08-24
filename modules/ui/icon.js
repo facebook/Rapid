837ae4b0cb36e0fd93ab5b110d89924ebb3ac312
Bryan Housel <bhousel@gmail.com>
2023-08-24 14:40:49 -0400

    When multiple icons appear as siblings, they need unique iconID (closes #1038)
    Previous selector `selectAll('svg.icon')` would target all icons but bind to a single icon
    New selector `selectAll('svg.icon-${iconID}')` will target only the icon we want in the list

--- modules/ui/icon.js
+++ modules/ui/icon.js
@@ -1,10 +1,12 @@
 export function uiIcon(href, klass = '') {
+  const iconID = href.replace('#', '');
+
   return function render(selection) {
-    selection.selectAll('svg.icon')
-      .data([href])
+    selection.selectAll(`svg.icon-${iconID}`)
+      .data([iconID], d => d)
       .enter()
       .append('svg')
-      .attr('class', 'icon' + (klass ? ` ${klass}` : ''))
+      .attr('class', `icon icon-${iconID}` + (klass ? ` ${klass}` : ''))
       .append('use')
       .attr('xlink:href', href);
   };
