67aa0ce053c3e7baf46d055531d9add20759123e
Lauren <lauren.noble3@gmail.com>
2023-10-19 11:13:47 -0400

    gaze function to watch all build data in progress

--- scripts/server.js
+++ scripts/server.js
@@ -4,10 +4,16 @@ import gaze from 'gaze/lib/gaze.js';
 import StaticServer from 'static-server/server.js';
 
 import { buildCSS } from './build_css.js';
+import { buildData } from './build_data.js';
 
 
 gaze(['css/**/*.css'], (err, watcher) => {
   watcher.on('all', () => buildCSS());
+});
+
+// added gaze watcher for all .js files
+gaze(['**/*.js'], (err, watcher) => {
+  watcher.on('all', () => buildData());
 });
 
 const server = new StaticServer({ rootPath: process.cwd(), port: 8080, followSymlink: true });
