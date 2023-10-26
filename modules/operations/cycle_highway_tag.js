b25c36859d0a75791bb7bb3639622cdf530d5ffc
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-10-26 13:00:07 +0100

    refactor: improve code structure and logic in operationCycleHighwayTag

--- modules/operations/cycle_highway_tag.js
+++ modules/operations/cycle_highway_tag.js
@@ -1,20 +1,40 @@
 import { utilArrayIdentical } from '@rapid-sdk/util';
+
 import { actionChangePreset } from '../actions';
-import { actionNoop } from '../actions/noop';
 import { KeyOperationBehavior } from '../behaviors/KeyOperationBehavior';
 
 let _wasSelectedIDs = [];
-let _defaultHighwayPresetIDs = [
-  'highway/residential',
+// let _wasPresetIDs = [];
+
+
+export function operationCycleHighwayTag(context, selectedIDs) {
+  const editor = context.systems.editor;
+  const graph = editor.staging.graph;
+  const l10n = context.systems.l10n;
+  const presets = context.systems.presets;
+
+  // Allow cycling through lines that match these highway presets
+  const allowHighwayPresetRegex = [
+    /^highway\/(motorway|trunk|primary|secondary|tertiary|unclassified|residential|living_street|service|track)/,
+    /^line$/
+  ];
+
+  const defaultHighwayPresetIDs = [
+    'highway/residential',
     'highway/service',
     'highway/track',
     'highway/unclassified',
     'highway/tertiary',
-    'line',
-];
+    'line'
+  ];
 
-let _defaultCrossingPresetIDs = [
-  'crossing/unmarked;crossing:markings=no',
+  // Allow cycling through crossings that match these presets
+  const allowCrossingPresetRegex = [
+    /^crossing\/(unmarked|marked|marked:zebra|marked:lines|marked:dashes|marked:ladder|marked:dots|marked:ladder:skewed)/,
+  ];
+
+  const defaultCrossingPresetIDs = [
+    'crossing/unmarked;crossing:markings=no',
     'crossing/marked;crossing:markings=yes',
     'crossing/marked;crossing:markings=zebra',
     'crossing/marked;crossing:markings=lines',
@@ -22,67 +42,69 @@ let _defaultCrossingPresetIDs = [
     'crossing/marked;crossing:markings=dashes',
     'crossing/marked;crossing:markings=dots',
     'crossing/marked;crossing:markings=ladder:skewed',
-];
-
-export function operationCycleHighwayTag(context, selectedIDs) {
-
-  const allowHighwayPresetRegex = [
-    /^highway\/(motorway|trunk|primary|secondary|tertiary|unclassified|residential|living_street|service|track)/,
-    /^line$/,
   ];
 
-  const allowCrossingPresetRegex = [
-    /^crossing\/(unmarked|marked|marked:zebra|marked:lines|marked:dashes|marked:ladder|marked:dots|marked:ladder:skewed)/,
-  ];
+  // Determine whether the selection contains highways or crossings
+  const isHighwaySelection = selectedIDs.some(entityID => {
+    const entity = graph.hasEntity(entityID);
+    const preset = presets.match(entity, graph);
+    return allowHighwayPresetRegex.some(regex => regex.test(preset.id));
+  });
+
+  // Select the appropriate preset IDs based on the selection
+  const presetIDs = isHighwaySelection ? defaultHighwayPresetIDs : defaultCrossingPresetIDs;
+
+  // Filter selected entities based on the preset regex
+  const entities = selectedIDs
+  .map(entityID => graph.hasEntity(entityID))
+  .filter(entity => {
+    const preset = presets.match(entity, graph);
+    if (allowHighwayPresetRegex.some(regex => regex.test(preset.id)) || allowCrossingPresetRegex.some(regex => regex.test(preset.id))) {
+      return true;
+    }
+    return false;
+  });
 
   // same selection as before?
   const isSameSelection = utilArrayIdentical(selectedIDs, _wasSelectedIDs);
-  const presetSystem = context.systems.presets;
 
-  // Gather current entities allowed to be cycled
-  const entities = selectedIDs
-    .map(entityID => context.hasEntity(entityID))
-    .filter(entity => {
-      const preset = presetSystem.match(entity, context.graph());
-      if (allowHighwayPresetRegex.some(regex => regex.test(preset.id))) {
-        return true;
-      } else if (allowCrossingPresetRegex.some(regex => regex.test(preset.id))) {
-        return true;
-      } else {
-        return false;
-      }
-    });
+  _wasSelectedIDs = selectedIDs.slice(); // copy
+  // _wasPresetIDs = Array.from(presetIDs);  // copy
 
-  _wasSelectedIDs = selectedIDs.slice();
 
   let operation = function() {
     if (!entities.length) return;
 
-    // If this is the same selection as before, and the previous edit was also a cycle-tags,
-    // skip this `perform`, then all tag updates will be coalesced into the previous edit.
-    const annotation = operation.annotation();
-    if (!isSameSelection || context.systems.edits.undoAnnotation() !== annotation) {
-      // Start with a no-op edit that will be replaced by all the tag updates we end up doing.
-      context.perform(actionNoop(), annotation);
-    }
+    // Pick the next preset..
+    const currPresetIDs = Array.from(presetIDs);
+    const currPreset = presets.match(entities[0], editor.staging.graph);
+    const index = currPreset ? currPresetIDs.indexOf(currPreset.id) : -1;
+    const newPresetID = currPresetIDs[(index + 1) % currPresetIDs.length];
+    const newPreset = presets.item(newPresetID);
 
-    // Pick the next preset based on the entity type
-    const entityType = entities[0].type; // Assuming all selected entities have the same type
-    const presetIDs = getPresetIDsForEntityType(entityType);
-    const currPreset = presetSystem.match(entities[0], context.graph());
-    const index = currPreset ? presetIDs.indexOf(currPreset.id) : -1;
-    const newPresetID = presetIDs[(index + 1) % presetIDs.length];
-    const newPreset = presetSystem.item(newPresetID);
+    editor.beginTransaction();
 
-    // Update all selected entities...
+    // Update all selected highways...
     for (const entity of entities) {
-      const oldPreset = presetSystem.match(entity, context.graph());
-      const action = actionChangePreset(entity.id, oldPreset, newPreset, true);
-      context.replace(action, annotation);
+      const oldPreset = presets.match(entity, editor.staging.graph);
+      const action = actionChangePreset(entity.id, oldPreset, newPreset, true /* skip field defaults */);
+      editor.perform(action);
     }
 
-    context.enter('select-osm', { selectedIDs: selectedIDs });  // reselect
+    // If this is the same selection as before, and the previous edit was also a cycle-tags,
+    // we can just replace the previous edit with this one.
+    const annotation = operation.annotation();
+    const options = { annotation: annotation, selectedIDs: selectedIDs };
+    if (isSameSelection && editor.getUndoAnnotation() === annotation) {
+      editor.commitAppend(options);  // Replace the previous cycle-tags edit
+    } else {
+      editor.commit(options);
+    }
+
+    editor.endTransaction();
+    context.enter('select-osm', { selection: { osm: selectedIDs }} );  // reselect
   };
+
 
   operation.available = function() {
     return entities.length > 0;
@@ -92,30 +114,24 @@ export function operationCycleHighwayTag(context, selectedIDs) {
     return false;
   };
 
+
   operation.tooltip = function() {
     const disabledReason = operation.disabled();
     return disabledReason ?
-      context.t(`operations.cycle_highway_tag.${disabledReason}`) :
-      context.t('operations.cycle_highway_tag.description');
+      l10n.t(`operations.cycle_highway_tag.${disabledReason}`) :
+      l10n.t('operations.cycle_highway_tag.description');
   };
+
 
   operation.annotation = function() {
-    return context.t('operations.cycle_highway_tag.annotation');
+    return l10n.t('operations.cycle_highway_tag.annotation');
   };
 
+
   operation.id = 'cycle_highway_tag';
-  operation.keys = [ '⇧' + context.t('operations.cycle_highway_tag.key') ];
-  operation.title = context.t('operations.cycle_highway_tag.title');
+  operation.keys = [ '⇧' + l10n.t('operations.cycle_highway_tag.key') ];
+  operation.title = l10n.t('operations.cycle_highway_tag.title');
   operation.behavior = new KeyOperationBehavior(context, operation);
 
   return operation;
-}
-
-function getPresetIDsForEntityType(entityType) {
-  if (entityType === 'way') {
-    return _defaultHighwayPresetIDs;
-  } else if (entityType === 'crossing') {
-    return _defaultCrossingPresetIDs;
-  }
-  return [];
 }
