1b4cf9b38bdc0dec2d71e21e6d7a105bec91503c
Bryan Housel <bhousel@gmail.com>
2023-11-22 11:03:41 -0500

    Test: continuing to test deploy action (allow on pull request too) (re: #1163)
    - This allows deploy action to run on pull requests too.
    - Also adjusts the environment variables and the BUILD_ID to make it easier to
    understand what the build is for.
    - It also embeds more information about the build in the Context.

--- scripts/predeploy.js
+++ scripts/predeploy.js
@@ -2,7 +2,7 @@
 import shell from 'shelljs';
 
 // This script should normally only be run from a GitHub deploy action.
-// It rewrites the urls in `index.html` to instead point to a unique 'distpath' in our S3 bucket.
+// It rewrites the urls in `index.html` to instead point to a unique 'buildID' in our S3 bucket.
 // Then we can just copy this `index.html` around and things will just work.
 // This is how we do deploys of Rapid.
 
@@ -12,21 +12,24 @@ const mm = ('0' + (now.getUTCMonth() + 1)).slice(-2);
 const dd = ('0' + now.getUTCDate()).slice(-2);
 
 // Get these things from environment (fallbacks for testing)
-const yyyymmdd = process.env.YYYYMMDD ?? `${yyyy}${mm}${dd}`;
-const revision = process.env.REVISION ?? 'deadc0de';     // normally the git short hash
-const distpath = process.env.DISTPATH ?? `${yyyymmdd}-${revision}`;
+const yyyymmdd = process.env.YYYYMMDD  ?? `${yyyy}${mm}${dd}`;
+const buildSHA = process.env.BUILD_SHA ?? 'deadc0de';     // normally the git short hash
+const buildID  = process.env.BUILD_ID  ?? `local-${buildSHA}`;
 
 let file = 'dist/index.html';
+let path = `/rapid/${buildID}`;
 
 // If you want to test this script, uncomment these lines to copy 'index.html' instead of modifying it in place.
 //shell.cp('-f', 'dist/index.html', 'dist/index-copy.html');
 //file = 'dist/index-copy.html';
 
-// Update `index.html` in place to use the distpath urls
-shell.sed('-i', 'dist/', `/rapid/${distpath}/`, file);
-shell.sed('-i', 'img/', `/rapid/${distpath}/img/`, file);
-shell.sed('-i', 'rapid.css', `/rapid/${distpath}/rapid.css`, file);
-shell.sed('-i', 'rapid.js', `/rapid/${distpath}/rapid.js`, file);
-shell.sed('-i', 'rapid.min.js', `/rapid/${distpath}/rapid.min.js`, file);
-shell.sed('-i', "context.assetPath = ''", `context.assetPath = '/rapid/${distpath}/'`, file);
-shell.sed('-i', "context.revision = ''", `context.revision = '${revision}'`, file);
+// Update `index.html` in place to use the buildID urls
+shell.sed('-i', 'dist/', `${path}/`, file);
+shell.sed('-i', 'img/', `${path}/img/`, file);
+shell.sed('-i', 'rapid.css', `${path}/rapid.css`, file);
+shell.sed('-i', 'rapid.js', `${path}/rapid.js`, file);
+shell.sed('-i', 'rapid.min.js', `${path}/rapid.min.js`, file);
+shell.sed('-i', /context.assetPath.*;/, `context.assetPath = '${path}/';`, file);
+shell.sed('-i', /context.buildID.*;/, `context.buildID = '${buildID}';`, file);
+shell.sed('-i', /context.buildSHA.*;/, `context.buildSHA = '${buildSHA}';`, file);
+shell.sed('-i', /context.buildDate.*;/, `context.buildDate = '${yyyymmdd}';`, file);
