eb38a38322cf1b211b6d95381a3574455ff58829
Bryan Housel <bhousel@gmail.com>
2023-11-22 12:59:45 -0500

    A few things useful for canary/dev builds - Swap `rapid.min.js` for unminified `rapid.js` - Make context global (i.e. `window.context`)

--- scripts/predeploy.js
+++ scripts/predeploy.js
@@ -16,20 +16,29 @@ const yyyymmdd = process.env.YYYYMMDD  ?? `${yyyy}${mm}${dd}`;
 const buildSHA = process.env.BUILD_SHA ?? 'deadc0de';     // normally the git short hash
 const buildID  = process.env.BUILD_ID  ?? `local-${buildSHA}`;
 
+const isDebug = /^(local|main|pull-request)-/.test(buildID);
+const path = `/rapid/${buildID}`;
+
 let file = 'dist/index.html';
-let path = `/rapid/${buildID}`;
 
 // If you want to test this script, uncomment these lines to copy 'index.html' instead of modifying it in place.
-//shell.cp('-f', 'dist/index.html', 'dist/index-copy.html');
+// Then run `node scripts/predeploy.js`
 //file = 'dist/index-copy.html';
+//shell.cp('-f', 'dist/index.html', file);
 
 // Update `index.html` in place to use the buildID urls
 shell.sed('-i', 'dist/', `${path}/`, file);
 shell.sed('-i', 'img/', `${path}/img/`, file);
 shell.sed('-i', 'rapid.css', `${path}/rapid.css`, file);
 shell.sed('-i', 'rapid.js', `${path}/rapid.js`, file);
-shell.sed('-i', 'rapid.min.js', `${path}/rapid.min.js`, file);
 shell.sed('-i', /context.assetPath.*;/, `context.assetPath = '${path}/';`, file);
 shell.sed('-i', /context.buildID.*;/, `context.buildID = '${buildID}';`, file);
 shell.sed('-i', /context.buildSHA.*;/, `context.buildSHA = '${buildSHA}';`, file);
 shell.sed('-i', /context.buildDate.*;/, `context.buildDate = '${yyyymmdd}';`, file);
+
+if (isDebug) {
+  shell.sed('-i', 'rapid.min.js', `${path}/rapid.js`, file);                      // don't use minified rapid
+  shell.sed('-i', 'const context =', 'const context = window.context =', file);   // make context global
+} else {
+  shell.sed('-i', 'rapid.min.js', `${path}/rapid.min.js`, file);
+}
