de310fada7237514ae30c458d27aa0430d6d5215
Tanner A. Wuster <tannerwuster@gmail.com>
2024-01-22 16:27:21 -0700

    converted noop.test.js

--- test/unit/actions/noop.test.js
+++ test/unit/actions/noop.test.js
@@ -3,7 +3,7 @@ import { strict as assert } from 'node:assert';
 import * as Rapid from '../../../modules/headless.js';
 
 test('actionNoop', async t => {
-  await t.test('does nothing', t => {
+  await t.test('does nothing', () => {
     const graph = new Rapid.Graph();
     const result = Rapid.actionNoop()(graph);
     assert.ok(result instanceof Rapid.Graph);
