60a1629615217eb0e45af0bd79d33dd85e489f45
Bryan Housel <bhousel@gmail.com>
2024-01-22 10:52:21 -0500

    Update discard_tags tests

--- test/unit/actions/discard_tags.test.js
+++ test/unit/actions/discard_tags.test.js
@@ -2,41 +2,57 @@ import { test } from 'node:test';
 import { strict as assert } from 'node:assert';
 import * as Rapid from '../../../modules/headless.js';
 
-const it = function() {};  // remove
-const expect = function() {};  // remove
-
 test.todo('actionDiscardTags', async t => {
-    var discardTags = { created_by: true };
+  const discardTags = { created_by: true };
 
-    it('discards obsolete tags from modified entities', function() {
-        var way = Rapid.osmWay({ id: 'w1', tags: { created_by: 'Potlatch' } });
-        var base = new Rapid.Graph([way]);
-        var head = base.replace(way.update({ tags: { created_by: 'Potlatch', foo: 'bar' } }));
-        var action = Rapid.actionDiscardTags(new Rapid.Difference(base, head), discardTags);
-        expect(action(head).entity(way.id).tags).to.eql({foo: 'bar'});
-    });
+  await t.test('defaults to empty discardTags', t => {
+    const way = Rapid.osmWay({ id: 'w1', tags: { created_by: 'Potlatch' } });
+    const base = new Rapid.Graph();
+    const head = base.replace(way);
+    const action = Rapid.actionDiscardTags(new Rapid.Difference(base, head));  // no discardTags
+    const result = action(head);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.equal(result.entity('w1'), way);
+  });
 
-    it('discards obsolete tags from created entities', function() {
-        var way = Rapid.osmWay({ tags: { created_by: 'Potlatch' } });
-        var base = new Rapid.Graph();
-        var head = base.replace(way);
-        var action = Rapid.actionDiscardTags(new Rapid.Difference(base, head), discardTags);
-        expect(action(head).entity(way.id).tags).to.eql({});
-    });
+  await t.test('discards obsolete tags from modified entities', t => {
+    const way = Rapid.osmWay({ id: 'w1', tags: { created_by: 'Potlatch' } });
+    const base = new Rapid.Graph([way]);
+    const head = base.replace(way.update({ tags: { created_by: 'Potlatch', foo: 'bar' } }));
+    const action = Rapid.actionDiscardTags(new Rapid.Difference(base, head), discardTags);
+    const result = action(head);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.deepEqual(result.entity('w1').tags, {foo: 'bar'});
+  });
 
-    it('doesn\'t modify entities without obsolete tags', function() {
-        var way = Rapid.osmWay();
-        var base = new Rapid.Graph();
-        var head = base.replace(way);
-        var action = Rapid.actionDiscardTags(new Rapid.Difference(base, head), discardTags);
-        expect(action(head).entity(way.id)).to.equal(way);
-    });
+  await t.test('discards obsolete tags from created entities', t => {
+    const way = Rapid.osmWay({ id: 'w1', tags: { created_by: 'Potlatch' } });
+    const base = new Rapid.Graph();
+    const head = base.replace(way);
+    const action = Rapid.actionDiscardTags(new Rapid.Difference(base, head), discardTags);
+    const result = action(head);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.deepEqual(result.entity('w1').tags, {});
+  });
 
-    it('discards tags with empty values', function() {
-        var way = Rapid.osmWay({ tags: { lmnop: '' } });
-        var base = new Rapid.Graph();
-        var head = base.replace(way);
-        var action = Rapid.actionDiscardTags(new Rapid.Difference(base, head), discardTags);
-        expect(action(head).entity(way.id).tags).to.eql({});
-    });
+  await t.test('doesn\'t modify entities without obsolete tags', t => {
+    const way = Rapid.osmWay({ id: 'w1' });
+    const base = new Rapid.Graph();
+    const head = base.replace(way);
+    const action = Rapid.actionDiscardTags(new Rapid.Difference(base, head), discardTags);
+    const result = action(head);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.equal(result.entity('w1'), way);
+  });
+
+  await t.test('discards tags with empty values', t => {
+    const way = Rapid.osmWay({ id: 'w1', tags: { lmnop: '' } });
+    const base = new Rapid.Graph();
+    const head = base.replace(way);
+    const action = Rapid.actionDiscardTags(new Rapid.Difference(base, head), discardTags);
+    const result = action(head);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.deepEqual(result.entity('w1').tags, {});
+  });
+
 });
