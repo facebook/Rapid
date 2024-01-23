7b8e797a1c11926c3830b78a3e7c9cd1a5a1ac67
Bryan Housel <bhousel@gmail.com>
2024-01-23 16:23:39 -0500

    Update merge, merge_node tests

--- test/unit/actions/merge.test.js
+++ test/unit/actions/merge.test.js
@@ -2,64 +2,66 @@ import { test } from 'node:test';
 import { strict as assert } from 'node:assert';
 import * as Rapid from '../../../modules/headless.js';
 
-const it = function() {};  // remove
-const expect = function() {};  // remove
 
-test.todo('actionMerge', async t => {
-    it('merges multiple points to a line', function () {
-        var graph = new Rapid.Graph([
-                Rapid.osmNode({id: 'a', tags: {a: 'a'}}),
-                Rapid.osmNode({id: 'b', tags: {b: 'b'}}),
-                Rapid.osmWay({id: 'w'}),
-                Rapid.osmRelation({id: 'r', members: [{id: 'a', role: 'r', type: 'node'}]})
-            ]),
-            action = Rapid.actionMerge(['a', 'b', 'w']);
+test('actionMerge', async t => {
+  await t.test('merges multiple points to a line', t => {
+    const graph = new Rapid.Graph([
+      Rapid.osmNode({id: 'a', tags: {a: 'a'}}),
+      Rapid.osmNode({id: 'b', tags: {b: 'b'}}),
+      Rapid.osmWay({id: 'w'}),
+      Rapid.osmRelation({id: 'r', members: [{id: 'a', role: 'r', type: 'node'}]})
+    ]);
 
-        expect(action.disabled(graph)).not.to.be.ok;
+    const action = Rapid.actionMerge(['a', 'b', 'w']);
+    assert.ok(!action.disabled(graph));
 
-        graph = action(graph);
+    const result = action(graph);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.ok(!result.hasEntity('a'));
+    assert.ok(!result.hasEntity('b'));
+    assert.deepEqual(result.entity('w').tags, {a: 'a', b: 'b'});
+    assert.deepEqual(result.entity('r').members, [{id: 'w', role: 'r', type: 'way'}]);
+  });
 
-        expect(graph.hasEntity('a')).to.be.undefined;
-        expect(graph.hasEntity('b')).to.be.undefined;
-        expect(graph.entity('w').tags).to.eql({a: 'a', b: 'b'});
-        expect(graph.entity('r').members).to.eql([{id: 'w', role: 'r', type: 'way'}]);
-    });
 
-    it('merges multiple points to an area', function () {
-        var graph = new Rapid.Graph([
-                Rapid.osmNode({id: 'a', tags: {a: 'a'}}),
-                Rapid.osmNode({id: 'b', tags: {b: 'b'}}),
-                Rapid.osmWay({id: 'w', tags: {area: 'yes'}}),
-                Rapid.osmRelation({id: 'r', members: [{id: 'a', role: 'r', type: 'node'}]})
-            ]),
-            action = Rapid.actionMerge(['a', 'b', 'w']);
+  await t.test('merges multiple points to an area', t => {
+    const graph = new Rapid.Graph([
+      Rapid.osmNode({id: 'a', tags: {a: 'a'}}),
+      Rapid.osmNode({id: 'b', tags: {b: 'b'}}),
+      Rapid.osmWay({id: 'w', tags: {area: 'yes'}}),
+      Rapid.osmRelation({id: 'r', members: [{id: 'a', role: 'r', type: 'node'}]})
+    ]);
 
-        expect(action.disabled(graph)).not.to.be.ok;
+    const action = Rapid.actionMerge(['a', 'b', 'w']);
+    assert.ok(!action.disabled(graph));
 
-        graph = action(graph);
+    const result = action(graph);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.ok(!result.hasEntity('a'));
+    assert.ok(!result.hasEntity('b'));
+    assert.deepEqual(result.entity('w').tags, {a: 'a', b: 'b', area: 'yes'});
+    assert.deepEqual(result.entity('r').members, [{id: 'w', role: 'r', type: 'way'}]);
+  });
 
-        expect(graph.hasEntity('a')).to.be.undefined;
-        expect(graph.hasEntity('b')).to.be.undefined;
-        expect(graph.entity('w').tags).to.eql({a: 'a', b: 'b', area: 'yes'});
-        expect(graph.entity('r').members).to.eql([{id: 'w', role: 'r', type: 'way'}]);
-    });
 
-    it('preserves original point if possible', function () {
-        var graph = new Rapid.Graph([
-                Rapid.osmNode({id: 'a', loc: [1, 0], tags: {a: 'a'}}),
-                Rapid.osmNode({id: 'p', loc: [0, 0], tags: {p: 'p'}}),
-                Rapid.osmNode({id: 'q', loc: [0, 1]}),
-                Rapid.osmWay({id: 'w', nodes: ['p', 'q'], tags: {w: 'w'}})
-            ]),
-            action = Rapid.actionMerge(['a', 'w']);
+  await t.test('preserves original point if possible', t => {
+    const graph = new Rapid.Graph([
+      Rapid.osmNode({id: 'a', loc: [1, 0], tags: {a: 'a'}}),
+      Rapid.osmNode({id: 'p', loc: [0, 0], tags: {p: 'p'}}),
+      Rapid.osmNode({id: 'q', loc: [0, 1]}),
+      Rapid.osmWay({id: 'w', nodes: ['p', 'q'], tags: {w: 'w'}})
+    ]);
 
-        graph = action(graph);
-        expect(graph.hasEntity('a')).to.be.ok;
-        expect(graph.hasEntity('p')).to.be.ok;
-        expect(graph.hasEntity('q')).to.be.undefined;
-        expect(graph.entity('w').tags).to.eql({a: 'a', w: 'w'});
-        expect(graph.entity('w').nodes).to.eql(['p', 'a']);
-        expect(graph.entity('a').loc[0]).to.eql(0);
-        expect(graph.entity('a').loc[1]).to.eql(1);
-    });
+    const action = Rapid.actionMerge(['a', 'w']);
+    assert.ok(!action.disabled(graph));
+
+    const result = action(graph);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.ok(result.hasEntity('a'));
+    assert.ok(result.hasEntity('p'));
+    assert.ok(!result.hasEntity('q'));
+    assert.deepEqual(result.entity('w').tags, {a: 'a', w: 'w'});
+    assert.deepEqual(result.entity('w').nodes, ['p', 'a']);
+    assert.deepEqual(result.entity('a').loc, [0, 1]);
+  });
 });
--- test/unit/actions/merge_nodes.test.js
+++ test/unit/actions/merge_nodes.test.js
@@ -2,173 +2,173 @@ import { test } from 'node:test';
 import { strict as assert } from 'node:assert';
 import * as Rapid from '../../../modules/headless.js';
 
-const it = function() {};  // remove
-const expect = function() {};  // remove
+test('actionMergeNodes', async t => {
 
-test.todo('actionMergeNodes', async t => {
+  await t.test('#disabled', async t => {
+    await t.test('enabled for both internal and endpoint nodes', t => {
+      //
+      // a --- b --- c
+      //
+      //       d
+      //       |
+      //       e
+      //
+      const graph = new Rapid.Graph([
+        Rapid.osmNode({ id: 'a', loc: [-2,  2] }),
+        Rapid.osmNode({ id: 'b', loc: [ 0,  2] }),
+        Rapid.osmNode({ id: 'c', loc: [ 2,  2] }),
+        Rapid.osmNode({ id: 'd', loc: [ 0,  0] }),
+        Rapid.osmNode({ id: 'e', loc: [ 0, -2] }),
+        Rapid.osmWay({ id: '-', nodes: ['a', 'b', 'c'] }),
+        Rapid.osmWay({ id: '|', nodes: ['d', 'e'] })
+      ]);
 
-    it('#disabled', function () {
-        it('enabled for both internal and endpoint nodes', function() {
-            //
-            // a --- b --- c
-            //
-            //       d
-            //       |
-            //       e
-            //
-            var graph = new Rapid.Graph([
-                Rapid.osmNode({ id: 'a', loc: [-2,  2] }),
-                Rapid.osmNode({ id: 'b', loc: [ 0,  2] }),
-                Rapid.osmNode({ id: 'c', loc: [ 2,  2] }),
-                Rapid.osmNode({ id: 'd', loc: [ 0,  0] }),
-                Rapid.osmNode({ id: 'e', loc: [ 0, -2] }),
-                Rapid.osmWay({ id: '-', nodes: ['a', 'b', 'c'] }),
-                Rapid.osmWay({ id: '|', nodes: ['d', 'e'] })
-            ]);
-
-            expect(Rapid.actionMergeNodes(['b', 'e']).disabled(graph)).to.be.not.ok;
-        });
+      const disabled = Rapid.actionMergeNodes(['b', 'e']).disabled(graph);
+      assert.ok(!disabled);
     });
+  });
 
 
-    it('merges two isolated nodes, averaging loc', function() {
-        var graph = new Rapid.Graph([
-            Rapid.osmNode({ id: 'a', loc: [0, 0] }),
-            Rapid.osmNode({ id: 'b', loc: [4, 4] })
-        ]);
+  await t.test('merges two isolated nodes, averaging loc', t => {
+    const graph = new Rapid.Graph([
+      Rapid.osmNode({ id: 'a', loc: [0, 0] }),
+      Rapid.osmNode({ id: 'b', loc: [4, 4] })
+    ]);
 
-        graph = Rapid.actionMergeNodes(['a', 'b'])(graph);
+    const result = Rapid.actionMergeNodes(['a', 'b'])(graph);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.ok(!result.hasEntity('a'));
 
-        expect(graph.hasEntity('a')).to.be.undefined;
-
-        var survivor = graph.hasEntity('b');
-        expect(survivor).to.be.an.instanceof(Rapid.osmNode);
-        expect(survivor.loc).to.eql([2, 2], 'average loc');
-    });
-
-
-    it('merges two isolated nodes, merging tags, and keeping loc of the interesting node', function() {
-        var graph = new Rapid.Graph([
-            Rapid.osmNode({ id: 'a', loc: [0, 0], tags: { highway: 'traffic_signals' }}),
-            Rapid.osmNode({ id: 'b', loc: [4, 4] })
-        ]);
-
-        graph = Rapid.actionMergeNodes(['a', 'b'])(graph);
-
-        expect(graph.hasEntity('a')).to.be.undefined;
-
-        var survivor = graph.hasEntity('b');
-        expect(survivor).to.be.an.instanceof(Rapid.osmNode);
-        expect(survivor.tags).to.eql({ highway: 'traffic_signals' }, 'merge all tags');
-        expect(survivor.loc).to.eql([0, 0], 'use loc of interesting node');
-    });
+    const survivor = result.hasEntity('b');
+    assert.ok(survivor instanceof Rapid.osmNode);
+    assert.deepEqual(survivor.loc, [2, 2], 'average loc of merged nodes');
+  });
 
 
-    it('merges two isolated nodes, merging tags, and averaging loc of both interesting nodes', function() {
-        var graph = new Rapid.Graph([
-            Rapid.osmNode({ id: 'a', loc: [0, -2], tags: { highway: 'traffic_signals' } }),
-            Rapid.osmNode({ id: 'b', loc: [0,  2], tags: { crossing: 'marked' } })
-        ]);
-        graph = Rapid.actionMergeNodes(['a', 'b'])(graph);
+  await t.test('merges two isolated nodes, merging tags, and keeping loc of the interesting node', t => {
+    const graph = new Rapid.Graph([
+      Rapid.osmNode({ id: 'a', loc: [0, 0], tags: { highway: 'traffic_signals' }}),
+      Rapid.osmNode({ id: 'b', loc: [4, 4] })
+    ]);
 
-        expect(graph.hasEntity('a')).to.be.undefined;
+    const result = Rapid.actionMergeNodes(['a', 'b'])(graph);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.ok(!result.hasEntity('a'));
 
-        var survivor = graph.hasEntity('b');
-        expect(survivor.tags).to.eql({ highway: 'traffic_signals', crossing: 'marked' }, 'merge all tags');
-        expect(survivor.loc).to.eql([0, 0], 'average loc of both interesting nodes');
-    });
-
-
-    it('merges two nodes along a single way', function() {
-        //
-        //  scenerio:         merge b,c:
-        //
-        //  a -- b -- c       a ---- c
-        //
-        var graph = new Rapid.Graph([
-            Rapid.osmNode({ id: 'a', loc: [-2,  2] }),
-            Rapid.osmNode({ id: 'b', loc: [ 0,  2] }),
-            Rapid.osmNode({ id: 'c', loc: [ 2,  2] }),
-            Rapid.osmWay({ id: '-', nodes: ['a', 'b', 'c'] })
-        ]);
-
-        graph = Rapid.actionMergeNodes(['b', 'c'])(graph);
-
-        expect(graph.hasEntity('b')).to.be.undefined;
-
-        var survivor = graph.hasEntity('c');
-        expect(survivor).to.be.an.instanceof(Rapid.osmNode);
-        expect(survivor.loc).to.eql([1, 2]);
-        expect(graph.parentWays(survivor).length).to.equal(1);
-    });
+    const survivor = result.hasEntity('b');
+    assert.ok(survivor instanceof Rapid.osmNode);
+    assert.deepEqual(survivor.tags, { highway: 'traffic_signals' });
+    assert.deepEqual(survivor.loc, [0, 0], 'use loc of survivor');
+  });
 
 
-    it('merges two nodes from two ways', function() {
-        //
-        //  scenerio:        merge b,d:
-        //
-        //  a -- b -- c      a -_   _- c
-        //                        d
-        //       d                |
-        //       |                |
-        //       e                e
-        //
-        var graph = new Rapid.Graph([
-            Rapid.osmNode({ id: 'a', loc: [-2,  2] }),
-            Rapid.osmNode({ id: 'b', loc: [ 0,  2] }),
-            Rapid.osmNode({ id: 'c', loc: [ 2,  2] }),
-            Rapid.osmNode({ id: 'd', loc: [ 0,  0] }),
-            Rapid.osmNode({ id: 'e', loc: [ 0, -2] }),
-            Rapid.osmWay({ id: '-', nodes: ['a', 'b', 'c'] }),
-            Rapid.osmWay({ id: '|', nodes: ['d', 'e'] })
-        ]);
+  await t.test('merges two isolated nodes, merging tags, and averaging loc of both interesting nodes', t => {
+    const graph = new Rapid.Graph([
+      Rapid.osmNode({ id: 'a', loc: [0, -2], tags: { highway: 'traffic_signals' } }),
+      Rapid.osmNode({ id: 'b', loc: [0,  2], tags: { crossing: 'marked' } })
+    ]);
 
-        graph = Rapid.actionMergeNodes(['b', 'd'])(graph);
+    const result = Rapid.actionMergeNodes(['a', 'b'])(graph);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.ok(!result.hasEntity('a'));
 
-        expect(graph.hasEntity('b')).to.be.undefined;
-
-        var survivor = graph.hasEntity('d');
-        expect(survivor).to.be.an.instanceof(Rapid.osmNode);
-        expect(survivor.loc).to.eql([0, 1]);
-        expect(graph.parentWays(survivor).length).to.equal(2);
-    });
+    const survivor = result.hasEntity('b');
+    assert.ok(survivor instanceof Rapid.osmNode);
+    assert.deepEqual(survivor.tags, { highway: 'traffic_signals', crossing: 'marked' }, 'merge all tags');
+    assert.deepEqual(survivor.loc, [0, 0], 'average loc of merged nodes');
+  });
 
 
-    it('merges three nodes from three ways', function () {
-        //
-        //  scenerio:        merge b,d:
-        //
-        //        c                c
-        //        |                |
-        //        d                |
-        //                         |
-        //  a --- b          a --- e
-        //                         ‖
-        //        e                ‖
-        //        ‖                ‖
-        //        f                f
-        //
-        var graph = new Rapid.Graph([
-            Rapid.osmNode({ id: 'a', loc: [-2,  0] }),
-            Rapid.osmNode({ id: 'b', loc: [ 0,  0] }),
-            Rapid.osmNode({ id: 'c', loc: [ 0,  4] }),
-            Rapid.osmNode({ id: 'd', loc: [ 0,  2] }),
-            Rapid.osmNode({ id: 'e', loc: [ 0, -2] }),
-            Rapid.osmNode({ id: 'f', loc: [ 0, -4] }),
-            Rapid.osmWay({ id: '-', nodes: ['a', 'b'] }),
-            Rapid.osmWay({ id: '|', nodes: ['c', 'd'] }),
-            Rapid.osmWay({ id: '‖', nodes: ['e', 'f'] })
-        ]);
+  await t.test('merges two nodes along a single way', t => {
+    //
+    //  scenerio:         merge b,c:
+    //
+    //  a -- b -- c       a ---- c
+    //
+    const graph = new Rapid.Graph([
+      Rapid.osmNode({ id: 'a', loc: [-2,  2] }),
+      Rapid.osmNode({ id: 'b', loc: [ 0,  2] }),
+      Rapid.osmNode({ id: 'c', loc: [ 2,  2] }),
+      Rapid.osmWay({ id: '-', nodes: ['a', 'b', 'c'] })
+    ]);
 
-        graph = Rapid.actionMergeNodes(['b', 'd', 'e'])(graph);
+    const result = Rapid.actionMergeNodes(['b', 'c'])(graph);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.ok(!result.hasEntity('b'));
 
-        expect(graph.hasEntity('b')).to.be.undefined;
-        expect(graph.hasEntity('d')).to.be.undefined;
+    const survivor = result.hasEntity('c');
+    assert.ok(survivor instanceof Rapid.osmNode);
+    assert.deepEqual(survivor.loc, [1, 2], 'average loc of merged nodes');
+    assert.equal(result.parentWays(survivor).length, 1);
+  });
 
-        var survivor = graph.hasEntity('e');
-        expect(survivor).to.be.an.instanceof(Rapid.osmNode);
-        expect(survivor.loc).to.eql([0, 0]);
-        expect(graph.parentWays(survivor).length).to.equal(3);
-    });
+
+  await t.test('merges two nodes from two ways', t => {
+    //
+    //  scenerio:        merge b,d:
+    //
+    //  a -- b -- c      a -_   _- c
+    //                        d
+    //       d                |
+    //       |                |
+    //       e                e
+    //
+    const graph = new Rapid.Graph([
+      Rapid.osmNode({ id: 'a', loc: [-2,  2] }),
+      Rapid.osmNode({ id: 'b', loc: [ 0,  2] }),
+      Rapid.osmNode({ id: 'c', loc: [ 2,  2] }),
+      Rapid.osmNode({ id: 'd', loc: [ 0,  0] }),
+      Rapid.osmNode({ id: 'e', loc: [ 0, -2] }),
+      Rapid.osmWay({ id: '-', nodes: ['a', 'b', 'c'] }),
+      Rapid.osmWay({ id: '|', nodes: ['d', 'e'] })
+    ]);
+
+    const result = Rapid.actionMergeNodes(['b', 'd'])(graph);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.ok(!result.hasEntity('b'));
+
+    const survivor = result.hasEntity('d');
+    assert.ok(survivor instanceof Rapid.osmNode);
+    assert.deepEqual(survivor.loc, [0, 1], 'average loc of merged nodes');
+    assert.equal(result.parentWays(survivor).length, 2);
+  });
+
+
+  await t.test('merges three nodes from three ways', t => {
+    //
+    //  scenerio:        merge b,d:
+    //
+    //        c                c
+    //        |                |
+    //        d                |
+    //                         |
+    //  a --- b          a --- e
+    //                         ‖
+    //        e                ‖
+    //        ‖                ‖
+    //        f                f
+    //
+    const graph = new Rapid.Graph([
+      Rapid.osmNode({ id: 'a', loc: [-2,  0] }),
+      Rapid.osmNode({ id: 'b', loc: [ 0,  0] }),
+      Rapid.osmNode({ id: 'c', loc: [ 0,  4] }),
+      Rapid.osmNode({ id: 'd', loc: [ 0,  2] }),
+      Rapid.osmNode({ id: 'e', loc: [ 0, -2] }),
+      Rapid.osmNode({ id: 'f', loc: [ 0, -4] }),
+      Rapid.osmWay({ id: '-', nodes: ['a', 'b'] }),
+      Rapid.osmWay({ id: '|', nodes: ['c', 'd'] }),
+      Rapid.osmWay({ id: '‖', nodes: ['e', 'f'] })
+    ]);
+
+    const result = Rapid.actionMergeNodes(['b', 'd', 'e'])(graph);
+    assert.ok(result instanceof Rapid.Graph);
+    assert.ok(!result.hasEntity('b'));
+    assert.ok(!result.hasEntity('d'));
+
+    const survivor = result.hasEntity('e');
+    assert.ok(survivor instanceof Rapid.osmNode);
+    assert.deepEqual(survivor.loc, [0, 0], 'average loc of merged nodes');
+    assert.equal(result.parentWays(survivor).length, 3);
+  });
 
 });
