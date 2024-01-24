882314b5de0d55c31231585df3529986b04d4bfa
Tanner A. Wuster <tannerwuster@gmail.com>
2024-01-24 13:11:39 -0700

    updated upgrade_tags

--- test/unit/actions/upgrade_tags.test.js
+++ test/unit/actions/upgrade_tags.test.js
@@ -1,13 +1,10 @@
-import { test } from 'node:test';
+import { describe, it } from 'node:test';
 import { strict as assert } from 'node:assert';
 import * as Rapid from '../../../modules/headless.js';
 
-// const it = function() {};  // remove
-// const expect = function() {};  // remove
 
-test('actionUpgradeTags', async t => {
-
-    await t.test('upgrades a tag', () => {
+describe('actionUpgradeTags', () => {
+    it('upgrades a tag', () => {
         const oldTags = { amenity: 'swimming_pool' };
         const newTags = { leisure: 'swimming_pool' };
         const entity = Rapid.osmEntity({ tags: { amenity: 'swimming_pool', name: 'Foo' }});
@@ -18,7 +15,7 @@ test('actionUpgradeTags', async t => {
         assert.deepEqual(result.entity(entity.id).tags, { leisure: 'swimming_pool', name: 'Foo' });
     });
 
-    await t.test('upgrades a tag combination', () => {
+    it('upgrades a tag combination', () => {
         const oldTags = { amenity: 'vending_machine', vending: 'news_papers' };
         const newTags = { amenity: 'vending_machine', vending: 'newspapers' };
         const entity = Rapid.osmEntity({ tags: { amenity: 'vending_machine', vending: 'news_papers', name: 'Foo' }});
@@ -28,7 +25,9 @@ test('actionUpgradeTags', async t => {
         assert.ok(result instanceof Rapid.Graph);
         assert.deepEqual(result.entity(entity.id).tags, { amenity: 'vending_machine', vending: 'newspapers', name: 'Foo' });
     });
-    await t.test('upgrades a tag with multiple replacement tags', () => {
+
+
+    it('upgrades a tag with multiple replacement tags', () => {
         const oldTags = { natural: 'marsh' };
         const newTags = { natural: 'wetland', wetland: 'marsh' };
         const entity = Rapid.osmEntity({ tags: { natural: 'marsh', name: 'Foo' }});
@@ -39,7 +38,8 @@ test('actionUpgradeTags', async t => {
         assert.deepEqual(result.entity(entity.id).tags, { natural: 'wetland', wetland: 'marsh', name: 'Foo' });
 });
 
-    await t.test('upgrades a tag and overrides an existing value', () => {
+
+    it('upgrades a tag and overrides an existing value', () => {
         const oldTags = { landuse: 'wood' };
         const newTags = { natural: 'wood' };
         const entity = Rapid.osmEntity({ tags: { landuse: 'wood', natural: 'wetland', name: 'Foo' }});
@@ -50,7 +50,8 @@ test('actionUpgradeTags', async t => {
         assert.deepEqual(result.entity(entity.id).tags, { natural: 'wood', name: 'Foo' });
     });
 
-    await t.test('upgrades a tag with no replacement tags', () => {
+
+    it('upgrades a tag with no replacement tags', () => {
         const oldTags = { highway: 'no' };
         const newTags = {};
         const entity = Rapid.osmEntity({ tags: { highway: 'no', name: 'Foo' }});
@@ -61,7 +62,8 @@ test('actionUpgradeTags', async t => {
         assert.deepEqual(result.entity(entity.id).tags, { name: 'Foo' });
     });
 
-    await t.test('upgrades a wildcard tag and moves the value', () => {
+
+    it('upgrades a wildcard tag and moves the value', () => {
         const oldTags = { color: '*' };
         const newTags = { colour: '$1' };
         const entity = Rapid.osmEntity({ tags: { color: 'red', name: 'Foo' }});
@@ -72,7 +74,8 @@ test('actionUpgradeTags', async t => {
         assert.deepEqual(result.entity(entity.id).tags, { colour: 'red', name: 'Foo' });
     });
 
-    await t.test('upgrades a tag with a wildcard replacement and adds a default value', () => {
+
+    it('upgrades a tag with a wildcard replacement and adds a default value', () => {
         const oldTags = { amenity: 'shop' };
         const newTags = { shop: '*' };
         const entity = Rapid.osmEntity({ tags: { amenity: 'shop', name: 'Foo' }});
@@ -83,7 +86,8 @@ test('actionUpgradeTags', async t => {
         assert.deepEqual(result.entity(entity.id).tags, { shop: 'yes', name: 'Foo' });
     });
 
-    await t.test('upgrades a tag with a wildcard replacement and maintains the existing value', () => {
+
+    it('upgrades a tag with a wildcard replacement and maintains the existing value', () => {
         const oldTags = { amenity: 'shop' };
         const newTags = { shop: '*' };
         const entity = Rapid.osmEntity({ tags: { amenity: 'shop', shop: 'supermarket', name: 'Foo' }});
@@ -94,7 +98,8 @@ test('actionUpgradeTags', async t => {
         assert.deepEqual(result.entity(entity.id).tags, { shop: 'supermarket', name: 'Foo' });
     });
 
-    await t.test('upgrades a tag with a wildcard replacement and replaces the existing "no" value', () => {
+
+    it('upgrades a tag with a wildcard replacement and replaces the existing "no" value', () => {
         const oldTags = { amenity: 'shop' };
         const newTags = { shop: '*' };
         const entity = Rapid.osmEntity({ tags: { amenity: 'shop', shop: 'no', name: 'Foo' }});
@@ -105,7 +110,8 @@ test('actionUpgradeTags', async t => {
         assert.deepEqual(result.entity(entity.id).tags, { shop: 'yes', name: 'Foo' });
     });
 
-    await t.test('upgrades a tag from a semicolon-delimited list that has one other value', () => {
+
+    it('upgrades a tag from a semicolon-delimited list that has one other value', () => {
         const oldTags = { cuisine: 'vegan' };
         const newTags = { 'diet:vegan': 'yes' };
         const entity = Rapid.osmEntity({ tags: { cuisine: 'italian;vegan', name: 'Foo' }});
@@ -116,7 +122,8 @@ test('actionUpgradeTags', async t => {
         assert.deepEqual(result.entity(entity.id).tags, { cuisine: 'italian', 'diet:vegan': 'yes', name: 'Foo' });
     });
 
-    await t.test('upgrades a tag from a semicolon-delimited list that has many other values', () => {
+
+    it('upgrades a tag from a semicolon-delimited list that has many other values', () => {
         const oldTags = { cuisine: 'vegan' };
         const newTags = { 'diet:vegan': 'yes' };
         const entity = Rapid.osmEntity({ tags: { cuisine: 'italian;vegan;regional;american', name: 'Foo' }});
@@ -127,7 +134,8 @@ test('actionUpgradeTags', async t => {
         assert.deepEqual(result.entity(entity.id).tags, { cuisine: 'italian;regional;american', 'diet:vegan': 'yes', name: 'Foo' });
     });
 
-    await t.test('upgrades a tag within a semicolon-delimited list without changing other values', () => {
+
+    it('upgrades a tag within a semicolon-delimited list without changing other values', () => {
         const oldTags = { leisure: 'ice_rink', sport: 'hockey' };
         const newTags = { leisure: 'ice_rink', sport: 'ice_hockey' };
         const entity = Rapid.osmEntity({ tags: { leisure: 'ice_rink', sport: 'curling;hockey;multi', name: 'Foo' }});
@@ -138,7 +146,8 @@ test('actionUpgradeTags', async t => {
         assert.deepEqual(result.entity(entity.id).tags, { leisure: 'ice_rink', name: 'Foo', sport: 'curling;ice_hockey;multi' });
     });
 
-    await t.test('upgrades an entire semicolon-delimited tag value', () => {
+
+    it('upgrades an entire semicolon-delimited tag value', () => {
         const oldTags = { vending: 'parcel_mail_in;parcel_pickup' };
         const newTags = { vending: 'parcel_pickup;parcel_mail_in' };
         const entity = Rapid.osmEntity({ tags: { vending: 'parcel_mail_in;parcel_pickup', name: 'Foo' }});
@@ -148,5 +157,4 @@ test('actionUpgradeTags', async t => {
         assert.ok(result instanceof Rapid.Graph);
         assert.deepEqual(result.entity(entity.id).tags, { vending: 'parcel_pickup;parcel_mail_in', name: 'Foo' });
     });
-
 });
