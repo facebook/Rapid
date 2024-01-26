749530642807c27ab0498446e04f3df0380db84b
Tanner A. Wuster <tannerwuster@gmail.com>
2024-01-26 11:20:20 -0700

    note test updated

--- test/unit/osm/note.test.js
+++ test/unit/osm/note.test.js
@@ -1,20 +1,30 @@
-describe('osmNote', function () {
-    it('returns a note', function () {
-        expect(Rapid.osmNote()).to.be.an.instanceOf(Rapid.osmNote);
-        expect(Rapid.osmNote().type).to.equal('note');
+import { describe, it} from 'node:test';
+import { strict as assert } from 'node:assert';
+import * as Rapid from '../../../modules/headless.js';
+
+describe('osmNote', () => {
+    it('returns a note', () => {
+        var note = Rapid.osmNote();
+        assert.ok(note instanceof Rapid.osmNote);
+        assert.equal(note.type, 'note');
     });
+
 
     describe('#extent', function() {
         it('returns a note extent', function() {
-            expect(Rapid.osmNote({loc: [5, 10]}).extent().equals(new sdk.Extent([5, 10], [5, 10]))).to.be.ok;
+            var note = Rapid.osmNote({loc: [5, 10]});
+            var extent = note.extent();
+            assert.deepEqual(extent, new Rapid.sdk.Extent([5, 10], [5, 10]));
         });
     });
+
 
     describe('#update', function() {
         it('returns an updated note', function() {
-
+            // TODO: Add test case for update method
         });
     });
+
 
     describe('#isNew', function() {
         it('returns true if a note is new', function() {
@@ -22,16 +32,19 @@ describe('osmNote', function () {
                 id: -1,
                 loc: [5, 10]
             });
-            expect(note.isNew()).to.be.true;
+            assert.strictEqual(note.isNew(), true);
         });
+
+
         it('returns false if a note is not new', function() {
             var note = Rapid.osmNote({
                 id: 1,
                 loc: [5, 10]
             });
-            expect(note.isNew()).to.be.false;
+            assert.strictEqual(note.isNew(), false);
         });
     });
+
 
     describe('#move', function() {
         it('returns an moved note', function() {
@@ -40,8 +53,7 @@ describe('osmNote', function () {
                 loc: [5, 5]
             });
             note = note.move([10, 10]);
-            expect(note.loc).to.eql([10, 10]);
+            assert.deepEqual(note.loc, [10, 10]);
         });
     });
-
 });
