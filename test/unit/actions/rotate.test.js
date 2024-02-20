59b61b1c79ab5b244deafb61ec6af1e61f59acf6
Tanner A. Wuster <tannerwuster@gmail.com>
2024-02-20 10:55:20 -0700

    rotate test added

--- test/unit/actions/rotate.test.js
+++ test/unit/actions/rotate.test.js
@@ -2,29 +2,39 @@ import { describe, it } from 'node:test';
 import { strict as assert } from 'node:assert';
 import * as Rapid from '../../../modules/headless.js';
 
+
+
+// This discrepancy is due to the precision of JavaScript's floating-point arithmetic.
+// The value 6.123233995736766e-17 is extremely close to 0, but it's not exactly 0 due to the limitations
+// of floating-point precision.
+// Helper function to check if two numbers are approximately equal
+function approxEqual(a, b, epsilon = 1e-6) {
+    return Math.abs(a - b) < epsilon;
+}
+
+
 describe('actionRotate', () => {
-  it('rotates nodes around the specified pivot by the specified angle', () => {
-    const node1 = new Rapid.osmNode({loc: [0, 0]});
-    const node2 = new Rapid.osmNode({loc: [1, 0]});
-    const node3 = new Rapid.osmNode({loc: [0, 1]});
-    const graph = new Rapid.Graph([node1, node2, node3]);
-    const rotateIds = [node1.id, node2.id];
-    const pivot = [0, 0];
-    const angle = Math.PI / 2;
+    it('rotates nodes around a pivot point', () => {
+        // Define your nodes and graph
+        const nodeA = Rapid.osmNode({ id: 'a', loc: [0, 0] });
+        const nodeB = Rapid.osmNode({ id: 'b', loc: [1, 0] });
+        const graph = new Rapid.Graph([nodeA, nodeB]);
+        const pivot = [0, 0];
+        const angle = Math.PI / 2;  // 90 degrees in radians
 
-    const projection = {
-      project: (coords) => coords,
-      invert: (coords) => coords,
-    };
+        // Mock projection
+        const projection = {
+            project: function(loc) { return loc; },
+            invert: function(loc) { return loc; }
+        };
 
-    const result = Rapid.actionRotate(rotateIds, pivot, angle, projection)(graph);
+        const newGraph = Rapid.actionRotate([nodeA.id, nodeB.id], pivot, angle, projection)(graph);
 
-    assert.ok(result instanceof Rapid.Graph);
-    if (result && result.entityCount) {
-      assert.equal(result.entityCount(), 3);
-      assert.deepEqual(result.entity(node1.id).loc, [0, -1]);
-      assert.deepEqual(result.entity(node2.id).loc, [1, 0]);
-      assert.deepEqual(result.entity(node3.id).loc, [-1, 0]);
-    }
-  });
+        assert.ok(newGraph.hasEntity(nodeA.id));
+        assert.ok(newGraph.hasEntity(nodeB.id));
+        assert.ok(approxEqual(newGraph.entity(nodeA.id).loc[0], 0));
+        assert.ok(approxEqual(newGraph.entity(nodeA.id).loc[1], 0));
+        assert.ok(approxEqual(newGraph.entity(nodeB.id).loc[0], 0));
+        assert.ok(approxEqual(newGraph.entity(nodeB.id).loc[1], 1));
+    });
 });
