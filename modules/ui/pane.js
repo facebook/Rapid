facebook/Rapid

commit 6265ac137e8c0273fd692b44cca2927df1e8886f

Force a render when toggling open a pane (background, map date, etc) (closes #1405)
The main fix here is in pane.js, to force a re-render just as the pane is
opening.  These panes already do listen for events that cause them to render,
but sometimes those renders are skipped if the content is collapsed or
invisible.
In the case of the photos overlay section, they were not receiving an event to let
them to know to redraw the checkboxes correctly.

This commit also includes a bunch of updates to source documentation
and some small style adjustments to the maproulette challenge id input field,
unrelated to the actual fix.

css/80_app.css
@@ -3153,6 +3153,16 @@ div.full-screen > button:focus {
   cursor: pointer;
 }
 
+.layer-list .list-item-maproulette > label > span {
+  flex: 1 1 auto;
+}
+.layer-list .list-item-maproulette .challenge-id {
+  color: #333;
+  height: 20px;
+  width: 100px;
+  cursor: pointer;
+}
+
 .ideditor[dir='rtl'] .list-item-data-browse svg {
     transform: rotateY(180deg);
 }

modules/ui/pane.js
@@ -60,15 +60,23 @@ export function uiPane(context, id) {
   pane.togglePane = function(d3_event) {
     if (d3_event) d3_event.preventDefault();
     _paneTooltip.hide();
-    const shown = !_paneSelection.classed('shown');
-    ui.togglePanes(shown ? _paneSelection : undefined);
 
-    // Rapid#655: Since firing the validator is so expensive,
-    // only do it when we're right about to open the validation pane.
-    if (pane.id === 'issues' && shown) {
-      validator.validateAsync();
+    const show = !_paneSelection.classed('shown');
+    ui.togglePanes(show ? _paneSelection : undefined);
+
+    // We are showing the pane, rerender its content
+    if (show) {
+      _paneSelection.selectAll('.pane-content')
+        .call(pane.renderContent);
+
+      // Rapid#655: Since firing the validator is so expensive,
+      // only do it when we're right about to open the validation pane.
+      if (pane.id === 'issues') {
+        validator.validateAsync();
+      }
     }
   };
+
 
   pane.renderToggleButton = function(selection) {
     if (!_paneTooltip) {
@@ -86,6 +94,7 @@ export function uiPane(context, id) {
       .call(_paneTooltip);
   };
 
+
   pane.renderContent = function(selection) {
     // override to fully customize content
     if (_sections) {
@@ -94,6 +103,7 @@ export function uiPane(context, id) {
       }
     }
   };
+
 
   pane.renderPane = function(selection) {
     _paneSelection = selection
@@ -113,7 +123,6 @@ export function uiPane(context, id) {
       .append('button')
       .on('click', hidePane)
       .call(uiIcon('#rapid-icon-close'));
-
 
     _paneSelection
       .append('div')

modules/core/PhotoSystem.js
@@ -309,9 +309,9 @@ export class PhotoSystem extends AbstractSystem {
       // If we're selecting a photo then make sure its layer is enabled too.
       scene.enableLayers(layerID);
 
-    // renderer is not yet listening to photochange, so just manually tell the renderer to select-style it, for now
-    scene.classData(layerID, photoID, 'selected');
-    scene.classData(layerID, photoID, 'active');
+      // renderer is not yet listening to photochange, so just manually tell the renderer to select-style it, for now
+      scene.classData(layerID, photoID, 'selected');
+      scene.classData(layerID, photoID, 'active');
 
       // Try to show the viewer with the image selected..
       service.startAsync()

modules/ui/disclosure.js
@@ -8,8 +8,8 @@ import { utilFunctor, utilRebind } from '../util/index.js';
 // A Disclosure consists of a toggleable Label and Content
 // Clicking on the label toggles the visibility of the content below it.
 //
-//   > Label     V Label
-//               Content
+//   > Label     ⋁ Label
+//                 Content
 //
 export function uiDisclosure(context, key) {
   const l10n = context.systems.l10n;

modules/ui/inspector.js
@@ -6,18 +6,19 @@ import { uiPresetList } from './preset_list.js';
 import { uiViewOnOSM } from './view_on_osm.js';
 
 
-// The Inspector is a UI component for viewing/editing OSM Entities in the sidebar.
-// It consists of two divs that can slide side to side (only one will be visible at a time):
-//
-// +--------+--------+
-// |        |        |
-// | Preset | Entity |
-// |  List  | Editor |
-// |        |        |
-// |        |        |
-// +--------+--------+
-//
-
+/**
+ * uiInspector
+ * The Inspector is a UI component for viewing/editing OSM Entities in the sidebar.
+ * It consists of two divs that can slide side to side (only one will be visible at a time):
+ *
+ * +--------+--------+
+ * |        |        |
+ * | Preset | Entity |
+ * |  List  | Editor |
+ * |        |        |
+ * |        |        |
+ * +--------+--------+
+ */
 export function uiInspector(context) {
   const editor = context.systems.editor;
   const validator = context.systems.validator;

modules/ui/map3d_viewer.js
@@ -4,8 +4,8 @@ import { select as d3_select } from 'd3-selection';
 
 import { uiCmd } from './cmd.js';
 
-/*
- * uiMap3dViewer is a ui panel containing a maplibre 3D Map for visualizing buildings, roads, and areas.
+/**
+ * uiMap3dViewer is a ui panel containing a maplibre 3D Map
  * @param {*} context
  * @returns
  */

modules/ui/sections/background_list.js
@@ -14,11 +14,30 @@ import { uiMap3dViewer } from '../map3d_viewer.js';
 import { uiSection } from '../section.js';
 
 
+/** uiSectionBackgroundList
+ *  This collapsable section displays a radio button list of background imagery.
+ *  (and some other checkboxes below it)
+ *  Each list item also adds star buttons so users can select their favorite items.
+ *  It lives in the Background Settings pane.
+ *
+ *  ⋁ Backgrounds
+ *    ○ Bing Maps Aerial    ☆
+ *    ○ Esri Wayback        ☆
+ *    ○ Esri World Imagery  ☆
+ *    …
+ *    ○ None                ☆
+ *    ○ Custom              …
+ *
+ *    ◻ Show Minimap
+ *    ◻ Show 3d Map
+ *    …
+ */
 export function uiSectionBackgroundList(context) {
   const imagery = context.systems.imagery;
   const l10n = context.systems.l10n;
   const map = context.systems.map;
   const storage = context.systems.storage;
+  const ui = context.systems.ui;
 
   const section = uiSection(context, 'background-list')
     .label(l10n.t('background.backgrounds'))
@@ -136,7 +155,7 @@ export function uiSectionBackgroundList(context) {
       .attr('type', 'checkbox')
       .on('change', d3_event => {
         d3_event.preventDefault();
-        context.systems.ui.info.toggle('background');
+        ui.info.toggle('background');
       });
 
     panelLabelEnter
@@ -158,7 +177,7 @@ export function uiSectionBackgroundList(context) {
       .attr('type', 'checkbox')
       .on('change', d3_event => {
         d3_event.preventDefault();
-        context.systems.ui.info.toggle('location');
+        ui.info.toggle('location');
       });
 
     locPanelLabelEnter
@@ -529,11 +548,8 @@ export function uiSectionBackgroundList(context) {
 
   const deferredOnMapDraw = debounce(onMapDraw, 1000, { leading: true, trailing: true });
 
-  imagery
-    .on('imagerychange', renderIfVisible);
-
-  map
-    .on('draw', deferredOnMapDraw);
+  imagery.on('imagerychange', renderIfVisible);
+  map.on('draw', deferredOnMapDraw);
 
   context.keybinding()
     .on(uiCmd('⌘' + l10n.t('background.key')), swapBackground)

modules/ui/sections/data_layers.js
@@ -7,19 +7,52 @@ import { uiSection } from '../section.js';
 import { uiSettingsCustomData } from '../settings/custom_data.js';
 
 
+/** uiSectionDataLayers
+ *  This collapsable section displays various checkboxes for toggleable data layers.
+ *  (and some other checkboxes below it)
+ *  There was some attempt made at grouping them logically.
+ *  It lives in the Map Data pane.
+ *
+ *  ⋁ Data Layers
+ *    ◻ OpenStreetMap Data
+ *    ◻ OpenStreetMap Notes
+ *
+ *    ◻ KeepRight Issues
+ *    ◻ ImproveOSM Issues
+ *    …
+ *
+ *    ◻ Custom Map Data      …
+ *
+ *    ◻ Show History Panel
+ *    ◻ Show Measurement Panel
+ */
 export function uiSectionDataLayers(context) {
   const l10n = context.systems.l10n;
+  const scene = context.scene();
+  const ui = context.systems.ui;
+
   const section = uiSection(context, 'data-layers')
     .label(l10n.t('map_data.data_layers'))
-    .disclosureContent(renderDisclosureContent);
+    .disclosureContent(render);
 
   const settingsCustomData = uiSettingsCustomData(context)
     .on('change', customChanged);
 
-  const scene = context.scene();
 
 
-  function renderDisclosureContent(selection) {
+  /* renderIfVisible
+   * This calls render on the Disclosure commponent.
+   * It skips actual rendering if the disclosure is closed
+   */
+  function renderIfVisible() {
+    section.reRender();
+  }
+
+
+  /* render
+   * Render the data layer list and the checkboxes below it
+   */
+  function render(selection) {
     let container = selection.selectAll('.data-layer-container')
       .data([0]);
 
@@ -182,9 +215,8 @@ export function uiSectionDataLayers(context) {
     labelEnter.filter(d => d.id === 'maproulette')
       .append('input')
       .attr('type', 'text')
-      .attr('placeholder', 'Enter challenge Id')
-      .attr('class', 'challenge-id-input')
-      .style('margin-left', '10px');
+      .attr('placeholder', 'challenge ID')
+      .attr('class', 'challenge-id');
 
     // Update
     li
@@ -319,7 +351,7 @@ export function uiSectionDataLayers(context) {
       .attr('type', 'checkbox')
       .on('change', d3_event => {
         d3_event.preventDefault();
-        context.systems.ui.info.toggle('history');
+        ui.info.toggle('history');
       });
 
     historyPanelLabelEnter
@@ -341,7 +373,7 @@ export function uiSectionDataLayers(context) {
       .attr('type', 'checkbox')
       .on('change', d3_event => {
         d3_event.preventDefault();
-        context.systems.ui.info.toggle('measurement');
+        ui.info.toggle('measurement');
       });
 
     measurementPanelLabelEnter
@@ -350,7 +382,7 @@ export function uiSectionDataLayers(context) {
   }
 
 
-  context.scene().on('layerchange', section.reRender);
+  scene.on('layerchange', renderIfVisible);
 
   return section;
 }

commit b5206ae343e923dc31c158efa9dd2290582f2a67

Fix issue causing help pane to append content on rerender
This bug was introduced in 6265ac137
The other panes are all written in a dynamic style so they can get re-rendered if necessary.
Help pane was the only one not like this, so on each render it would just append another copy of everything.

css/80_app.css
@@ -3916,7 +3916,7 @@ li.issue-fix-item button:not(.actionable) .fix-icon {
     }
 }
 
-.help-pane .toc li a.selected {
+.help-pane .toc li.selected {
     background: #e8ebff;
 }
 

modules/ui/pane.js
@@ -49,10 +49,6 @@ export function uiPane(context, id) {
     return pane;
   };
 
-  pane.selection = function() {
-    return _paneSelection;
-  };
-
   function hidePane() {
     ui.togglePanes();
   }

modules/ui/panes/help.js
@@ -1,3 +1,4 @@
+import { select as d3_select } from 'd3-selection';
 import { marked } from 'marked';
 
 import { uiIcon } from '../icon.js';
@@ -10,6 +11,9 @@ import { helpHtml } from '../intro/helper.js';
 export function uiPaneHelp(context) {
   const l10n = context.systems.l10n;
   const ui = context.systems.ui;
+
+  let _selection = null;
+  let _currIndex = 0;
 
   const docSections = [
     ['help', [
@@ -287,94 +291,37 @@ export function uiPaneHelp(context) {
     .iconName('rapid-icon-help');
 
 
-  helpPane.renderContent = function(content) {
 
-    function clickHelp(d, i) {
-      const isRTL = l10n.isRTL();
-      content.property('scrollTop', 0);
-      helpPane.selection().select('.pane-heading h2').text(d.title);
+  /**
+   * renderContent
+   * All panes have a `renderContent` function that will render this pane's
+   * content into the given parent selection.
+   * (this is the render function)
+   */
+  helpPane.renderContent = function(selection) {
+    _selection = selection;
 
-      body.html(d.contentHtml);
-      body.selectAll('a').attr('target', '_blank');
-      menuItems.classed('selected', item => item.title === d.title);
-
-      nav.html('');
-      if (isRTL) {
-        nav.call(drawNext).call(drawPrevious);
-      } else {
-        nav.call(drawPrevious).call(drawNext);
-      }
-
-
-      function drawNext(selection) {
-        if (i === docs.length - 1) return;
-
-        let nextLink = selection
-          .append('a')
-          .attr('href', '#')
-          .attr('class', 'next')
-          .on('click', function(d3_event) {
-            d3_event.preventDefault();
-            clickHelp(docs[i + 1], i + 1);
-          });
-
-        nextLink
-          .append('span')
-          .text(docs[i + 1].title)
-          .call(uiIcon((isRTL ? '#rapid-icon-backward' : '#rapid-icon-forward'), 'inline'));
-      }
-
-
-      function drawPrevious(selection) {
-        if (i === 0) return;
-
-        let prevLink = selection
-          .append('a')
-          .attr('href', '#')
-          .attr('class', 'previous')
-          .on('click', function(d3_event) {
-            d3_event.preventDefault();
-            clickHelp(docs[i - 1], i - 1);
-          });
-
-        prevLink
-          .call(uiIcon((isRTL ? '#rapid-icon-forward' : '#rapid-icon-backward'), 'inline'))
-          .append('span')
-          .text(docs[i - 1].title);
-      }
-    }
-
-
-    function clickWalkthrough(d3_event) {
-      d3_event.preventDefault();
-      if (context.inIntro) return;
-      context.container().call(uiIntro(context));
-      ui.togglePanes();
-    }
-
-
-    function clickShortcuts(d3_event) {
-      d3_event.preventDefault();
-      ui.shortcuts.show();
-    }
-
-    let toc = content   // table of contents
+    // table of contents
+    const tocEnter = selection.selectAll('.toc')
+      .data([0])
+      .enter()
       .append('ul')
       .attr('class', 'toc');
 
-    let menuItems = toc.selectAll('li')
+    tocEnter.selectAll('li')
       .data(docs)
       .enter()
       .append('li')
       .append('a')
       .attr('href', '#')
       .text(d => d.title)
-      .on('click', function(d3_event, d) {
+      .on('click', (d3_event, d) => {
         d3_event.preventDefault();
-        clickHelp(d, docs.indexOf(d));
+        clickChapter(d, docs.indexOf(d));
       });
 
-    let shortcuts = toc
+    // button for the shortcuts
+    const shortcutsEnter = tocEnter
       .append('li')
       .attr('class', 'shortcuts')
       .call(uiTooltip(context)
@@ -386,41 +333,129 @@ export function uiPaneHelp(context) {
       .attr('href', '#')
       .on('click', clickShortcuts);
 
-    shortcuts
+    shortcutsEnter
       .append('div')
       .text(l10n.t('shortcuts.title'));
 
-    let walkthrough = toc
+    // button for the walkthrough
+    const walkthroughEnter = tocEnter
       .append('li')
       .attr('class', 'walkthrough')
       .append('a')
       .attr('href', '#')
       .on('click', clickWalkthrough);
 
-    walkthrough
+    walkthroughEnter
       .append('svg')
       .attr('class', 'logo logo-walkthrough')
       .append('use')
       .attr('xlink:href', '#rapid-logo-walkthrough');
 
-    walkthrough
+    walkthroughEnter
       .append('div')
       .text(l10n.t('splash.walkthrough'));
 
-    let helpContent = content
+    // help content (everything that's not the table of contents)
+    const wrapEnter = selection.selectAll('.help-content-wrap')
+      .data([0])
+      .enter()
       .append('div')
-      .attr('class', 'left-content');
+      .attr('class', 'help-content-wrap');
 
-    let body = helpContent
+    wrapEnter
       .append('div')
-      .attr('class', 'body');
+      .attr('class', 'help-content');
 
-    let nav = helpContent
+    wrapEnter
       .append('div')
       .attr('class', 'nav');
 
-    clickHelp(docs[0], 0);
+    clickChapter(docs[_currIndex], _currIndex);
   };
+
+
+  /**
+   * clickChapter
+   */
+  function clickChapter(d, i) {
+    if (!_selection) return;  // called too early
+
+    _currIndex = i;
+
+    const isRTL = l10n.isRTL();
+    _selection.property('scrollTop', 0);
+
+    const helpPane = d3_select(_selection.node().parentElement);
+    helpPane.selectAll('.pane-heading > h2').text(d.title);
+
+    const content = _selection.selectAll('.help-content');
+    content.html(d.contentHtml);
+    content.selectAll('a').attr('target', '_blank');  // outbound links should open in new tab
+
+    _selection.selectAll('.toc > li')
+      .classed('selected', item => item === d);
+
+    const nav = _selection.selectAll('.nav');
+    nav.html('');    // empty innerHtml and replace it
+    if (isRTL) {
+      nav.call(drawNext).call(drawPrevious);
+    } else {
+      nav.call(drawPrevious).call(drawNext);
+    }
+
+
+    function drawNext(selection) {
+      if (i === docs.length - 1) return;
+
+      const nextLink = selection
+        .append('a')
+        .attr('href', '#')
+        .attr('class', 'next')
+        .on('click', d3_event => {
+          d3_event.preventDefault();
+          clickChapter(docs[i + 1], i + 1);
+        });
+
+      nextLink
+        .append('span')
+        .text(docs[i + 1].title)
+        .call(uiIcon((isRTL ? '#rapid-icon-backward' : '#rapid-icon-forward'), 'inline'));
+    }
+
+
+    function drawPrevious(selection) {
+      if (i === 0) return;
+
+      const prevLink = selection
+        .append('a')
+        .attr('href', '#')
+        .attr('class', 'previous')
+        .on('click', d3_event => {
+          d3_event.preventDefault();
+          clickChapter(docs[i - 1], i - 1);
+        });
+
+      prevLink
+        .call(uiIcon((isRTL ? '#rapid-icon-forward' : '#rapid-icon-backward'), 'inline'))
+        .append('span')
+        .text(docs[i - 1].title);
+    }
+  }
+
+
+  function clickWalkthrough(d3_event) {
+    d3_event.preventDefault();
+    if (context.inIntro) return;
+    context.container().call(uiIntro(context));
+    ui.togglePanes();
+  }
+
+
+  function clickShortcuts(d3_event) {
+    d3_event.preventDefault();
+    ui.shortcuts.show();
+  }
+
 
   return helpPane;
 }