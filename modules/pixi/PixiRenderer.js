dcc9e501c21b11c61774506ad0e6e0076e3c892f
Tanner A. Wuster <tannerwuster@gmail.com>
2024-07-29 10:41:09 -0600

    updated renderer for async init and new name schemes

--- modules/pixi/PixiRenderer.js
+++ modules/pixi/PixiRenderer.js
@@ -1,4 +1,6 @@
 import * as PIXI from 'pixi.js';
+import { Application, BitmapFont} from 'pixi.js';
+import { settings } from '@pixi/settings';
 import { EventEmitter } from '@pixi/utils';
 import { TAU, Viewport, numWrap, vecEqual, vecLength, vecRotate, vecScale, vecSubtract } from '@rapid-sdk/math';
 
@@ -68,15 +70,32 @@ export class PixiRenderer extends EventEmitter {
     this._tick = this._tick.bind(this);
 
     // Disable mipmapping, we always want textures near the resolution they are at.
-    PIXI.BaseTexture.defaultOptions.mipmap = PIXI.MIPMAP_MODES.OFF;
+    settings.AUTO_GENERATE_MIPMAPS = false;
+    // Asynchronously initialize the PixiJS application
+    this.initPixiApp();
 
-    // Prefer WebGL2, though browsers still may give us a WebGL1 context, see #493, #568
-    // Can also swap the commented lines below to force WebGL1 context for testing.
-    PIXI.settings.PREFER_ENV = PIXI.ENV.WEBGL2;
-    // PIXI.settings.PREFER_ENV = PIXI.ENV.WEBGL;
+// todo - we should stop doing this.. Access to pixi app should be via an instance of PixiRenderer
+// so we can have multiple Pixi renderers - this will make the minimap less hacky & enable restriction editor
+    context.pixi = this.pixi;
 
-    // Create a Pixi application rendering to the given surface `canvas`
-    this.pixi = new PIXI.Application({
+    // Prepare a basic bitmap font that we can use for things like debug messages
+    BitmapFont.install({
+        name: 'TitleFont',
+        textStyle: {
+            fontFamily: 'Arial',
+            fontSize: 12,
+            strokeThickness: 2,
+            fill: 'purple'
+        },
+        chars: BitmapFont.ALPHANUMERIC // Assuming you still specify characters
+    });
+
+  }
+
+
+  async initPixiApp() {
+    this.pixi = new Application();
+    await this.pixi.init({
       antialias: true,
       autoDensity: true,
       autoStart: false,        // don't start the ticker yet
@@ -90,7 +109,7 @@ export class PixiRenderer extends EventEmitter {
       resolution: window.devicePixelRatio,
       sharedLoader: true,
       sharedTicker: true,
-      view: surface.node()
+      view: this.surface.node()
     });
 
     window.__PIXI_DEVTOOLS__ = {
@@ -105,36 +124,17 @@ export class PixiRenderer extends EventEmitter {
     // https://github.com/bfanger/pixi-inspector
     globalThis.__PIXI_APP__ = this.pixi;
 
-// todo - we should stop doing this.. Access to pixi app should be via an instance of PixiRenderer
-// so we can have multiple Pixi renderers - this will make the minimap less hacky & enable restriction editor
-    context.pixi = this.pixi;
-
-    // Prepare a basic bitmap font that we can use for things like debug messages
-    PIXI.BitmapFont.from('debug', {
-      fill: 0xffffff,
-      fontSize: 14,
-      stroke: 0x333333,
-      strokeThickness: 2
-    },{
-      chars: PIXI.BitmapFont.ASCII,
-      padding: 0,
-      resolution: 2
-    });
-
-    // Setup the Ticker
-    // Replace the default Ticker listener (which just renders the scene each frame)
-    // with our own listener that gathers statistics and renders only as needed
-    const ticker = this.pixi.ticker;
-    const defaultListener = ticker._head.next;
-    ticker.remove(defaultListener.fn, defaultListener.context);
-    ticker.add(this._tick, this);
-    ticker.start();
-
+    // Setup the stage, ticker, and other components after initialization
+    this.setupStage();
+    this.setupTicker();
+    this.setupScenesAndEvents();
+  }
+  setupStage() {
     // Setup the stage
     // The `stage` should be positioned so that `[0,0]` is at the center of the viewport,
     // and this is the pivot point for map rotation.
     const stage = this.pixi.stage;
-    stage.name = 'stage';
+    stage.label = 'stage';
     stage.sortableChildren = true;
     stage.eventMode = 'static';
     // Add a big hit area to `stage` so that clicks on nothing will generate events
@@ -145,13 +145,24 @@ export class PixiRenderer extends EventEmitter {
     // so `project/unproject` continues to work.
     // This also includes the `offset` which includes any panning that the user has done.
     const origin = new PIXI.Container();
-    origin.name = 'origin';
+    origin.label = 'origin';
     origin.sortableChildren = true;
     origin.eventMode = 'passive';
     stage.addChild(origin);
     this.origin = origin;
+  }
 
-    // Setup other classes
+
+  setupTicker() {
+    const ticker = this.pixi.ticker;
+    const defaultListener = ticker._head.next;
+    ticker.remove(defaultListener.fn, defaultListener.context);
+    ticker.add(this._tick, this);
+    ticker.start();
+  }
+
+
+  setupScenesAndEvents() {
     this.scene = new PixiScene(this);
     this.events = new PixiEvents(this);
 
@@ -159,7 +170,7 @@ export class PixiRenderer extends EventEmitter {
     // This is because it will start loading assets and Pixi's asset loader is not reentrant.
     // (it causes test failures if we create a bunch of these)
     if (!_sharedTextures) {
-      _sharedTextures = new PixiTextures(context);
+        _sharedTextures = new PixiTextures(this.context);
     }
     this.textures = _sharedTextures;
   }
