facebook/Rapid

commit a31bf0756acde5d1f1eba4b0cb2211d05e37a163

Updates to Keyboard Shortcuts following #1606, #1592
Because we added a bunch of new keyboard shortcuts for toggling streetview
layers, the Keyboard Shortcuts screen got pretty cluttered.  This change
includes a few things to make our keyboard shortcuts easier to work with.

- Allow localization for the new layer toggling shortcuts introduced
  in #1606, #1592 , also some code cleanup
- Move all keyboard handlers into the UI components.
  (Core systems like Map and Photos start a bit earlier and aren't really
  intended to be localized in the way the UI components are)
- Rearrange the Keyboard Shortcuts screen content so everything fits better
- Move all the localizible keys into one place in core.yaml
  Before they were all over the place.
  (I believe it's ok to move these now since per 474211d3 it seems
  that Translation Memory will restore them if their keys change)
- Also starting to add more "developer notes" comments to the core.yaml file.
  Comments in this file become notes that translators can see!

modules/ui/sections/data_layers.js
@@ -28,7 +28,7 @@ import { utilCmd } from '../../util/cmd.js';
  */
 export function uiSectionDataLayers(context) {
   const l10n = context.systems.l10n;
-  const scene = context.scene();
+  const scene = context.systems.gfx.scene;
   const ui = context.systems.ui;
 
   const section = uiSection(context, 'data-layers')
@@ -38,31 +38,64 @@ export function uiSectionDataLayers(context) {
   const settingsCustomData = uiSettingsCustomData(context)
     .on('change', customChanged);
 
-  let previousLayerStates = new Map();
-  function toggleAllLayers() {
-    const allLayerIDs = ['osm', 'notes', 'rapid', 'maproulette', 'keepRight', 'osmose', 'geoScribble', 'custom-data', 'mapillary', 'streetside', 'kartaview'];
+  let _previousLayerStates = new Map();
+
+
+  /* toggleAllLayers
+   * @param  {Event} e - event that triggered the toggle (if any)
+   */
+  function toggleAllLayers(e) {
+    if (e)  e.preventDefault();
+
+    const allLayerIDs = [
+      'osm', 'notes', 'rapid', 'maproulette', 'keepRight', 'osmose', 'geoScribble',
+      'custom-data', 'mapillary', 'streetside', 'kartaview'
+    ];
+
     const anyLayerEnabled = allLayerIDs.some(layerID => showsLayer(layerID));
     if (anyLayerEnabled) {
       // Save current state and disable all layers
       allLayerIDs.forEach(layerID => {
-        previousLayerStates.set(layerID, showsLayer(layerID));
+        _previousLayerStates.set(layerID, showsLayer(layerID));
         setLayer(layerID, false);
       });
     } else {
       // Restore previous state
-      previousLayerStates.forEach((enabled, layerID) => {
+      _previousLayerStates.forEach((enabled, layerID) => {
         setLayer(layerID, enabled);
       });
     }
   }
 
-  // keyboard shortcut for toggling all layers
-  context.keybinding()
-    .on('⇧D', e => {
-      e.preventDefault();
-      toggleAllLayers();
-    });
 
+  /* toggleLayerKey
+   * Just wraps calls to `toggleLayer`, cancelling the key event
+   * @param  {Event} e - event that triggered the toggle (if any)
+   */
+  function toggleLayerKey(e, layerID) {
+    if (e)  e.preventDefault();
+    toggleLayer(layerID);
+  }
+
+  // setup key shortcuts
+  const toggleAllKey = utilCmd('⇧' + l10n.t('shortcuts.command.toggle_all_layers.key'));
+  const toggleOsmKey = utilCmd('⇧' + l10n.t('shortcuts.command.toggle_osm_data.key'));
+  const toggleNotesKey = utilCmd('⇧' + l10n.t('shortcuts.command.toggle_osm_notes.key'));
+  const toggleMapillaryKey = utilCmd('⇧' + l10n.t('shortcuts.command.toggle_mapillary.key'));
+  const toggleStreetsideKey = utilCmd('⇧' + l10n.t('shortcuts.command.toggle_streetside.key'));
+  const toggleKartaviewKey = utilCmd('⇧' + l10n.t('shortcuts.command.toggle_kartaview.key'));
+
+  context.keybinding().off([
+    toggleAllKey, toggleOsmKey, toggleNotesKey, toggleMapillaryKey, toggleStreetsideKey, toggleKartaviewKey
+  ]);
+
+  context.keybinding()
+    .on(toggleAllKey, e => toggleAllLayers(e))
+    .on(toggleOsmKey, e => toggleLayerKey(e, 'osm'))
+    .on(toggleNotesKey, e => toggleLayerKey(e, 'notes'))
+    .on(toggleMapillaryKey, e => toggleLayerKey(e, 'mapillary'))
+    .on(toggleStreetsideKey, e => toggleLayerKey(e, 'streetside'))
+    .on(toggleKartaviewKey, e => toggleLayerKey(e, 'kartaview'));
 
 
   /* renderIfVisible
@@ -141,8 +174,11 @@ export function uiSectionDataLayers(context) {
 
 
   function drawBaseItems(selection) {
-    const osmKeys = ['osm', 'notes', 'rapid'];
-    const osmLayers = osmKeys.map(layerID => scene.layers.get(layerID)).filter(Boolean);
+    const items = [
+      { id: 'osm',   layer: scene.layers.get('osm'),   key: 'shortcuts.command.toggle_osm_data.key' },
+      { id: 'notes', layer: scene.layers.get('notes'), key: 'shortcuts.command.toggle_osm_notes.key' },
+      { id: 'rapid', layer: scene.layers.get('rapid'), key: 'shortcuts.command.toggle_rapid_data.key' }
+    ];
 
     let ul = selection
       .selectAll('.layer-list-osm')
@@ -154,7 +190,7 @@ export function uiSectionDataLayers(context) {
       .merge(ul);
 
     let li = ul.selectAll('.list-item')
-      .data(osmLayers);
+      .data(items);
 
     li.exit()
       .remove();
@@ -169,7 +205,7 @@ export function uiSectionDataLayers(context) {
         d3_select(nodes[i])
           .call(uiTooltip(context)
             .title(l10n.t(`map_data.layers.${d.id}.tooltip`))
-            .shortcut(utilCmd('⇧' + l10n.t(`map_data.layers.${d.id}.key`)))
+            .shortcut(utilCmd('⇧' + l10n.t(d.key)))
             .placement('bottom')
           );
       });
@@ -177,7 +213,7 @@ export function uiSectionDataLayers(context) {
     labelEnter
       .append('input')
       .attr('type', 'checkbox')
-      .on('change', (d3_event, d) => toggleLayer(d.id));
+      .on('change', (e, d) => toggleLayer(d.id));
 
     labelEnter
       .append('span')
@@ -223,7 +259,7 @@ export function uiSectionDataLayers(context) {
     labelEnter
       .append('input')
       .attr('type', 'checkbox')
-      .on('change', (d3_event, d) => toggleLayer(d.id));
+      .on('change', (e, d) => toggleLayer(d.id));
 
     labelEnter
       .append('span')
@@ -380,7 +416,7 @@ export function uiSectionDataLayers(context) {
       .append('label')
       .call(uiTooltip(context)
         .title(l10n.t('map_data.history_panel.tooltip'))
-        .shortcut(utilCmd('⌘⇧' + l10n.t('info_panels.history.key')))
+        .shortcut(utilCmd('⌘⇧' + l10n.t('shortcuts.command.toggle_history_card.key')))
         .placement('top')
       );
 
@@ -399,7 +435,7 @@ export function uiSectionDataLayers(context) {
       .append('label')
       .call(uiTooltip(context)
         .title(l10n.t('map_data.measurement_panel.tooltip'))
-        .shortcut(utilCmd('⌘⇧' + l10n.t('info_panels.measurement.key')))
+        .shortcut(utilCmd('⌘⇧' + l10n.t('shortcuts.command.toggle_measurement_card.key')))
         .placement('top')
       );
 

css/80_app.css
@@ -4911,50 +4911,46 @@ li.issue-fix-item button:not(.actionable) .fix-icon {
   }
 }
 
-.modal-shortcuts .shortcut-tab {
+.shortcut-tab {
   display: flex;
   flex-flow: row wrap;
   justify-content: space-around;
 }
 
-.modal-shortcuts .shortcut-column {
+.shortcut-column {
   width: auto;
+  height: 0;
+  margin-bottom: 20px;
 }
 
-.modal-shortcuts .shortcut-tab-tools .shortcut-column {
-  flex: 1 1 100%;
-  width: 100%;
+.shortcut-row {
 }
 
-.modal-shortcuts td {
+.shortcut-row > td {
   padding-bottom: 3px;
 }
 
-.modal-shortcuts .shortcut-section {
-  padding: 20px 0 3px 0;
+.shortcut-row > .shortcut-section {
+  padding: 15px 0 5px 0;
 }
 
-.modal-shortcuts .shortcut-keys {
+.shortcut-keys {
   padding: 0 10px;
   color: #767676;
   text-align: right;
   white-space: nowrap;
 }
-.ideditor[dir='rtl'] .modal-shortcuts .shortcut-keys {
+.ideditor[dir='rtl'] .shortcut-keys {
   text-align: left;
 }
 
-.modal-shortcuts .shortcut-keys kbd {
+.shortcut-keys kbd {
   color: #555;
+  margin: unset;
 }
-.modal-shortcuts .shortcut-keys svg {
+.shortcut-separator {
   margin: 0 3px;
 }
-.modal-shortcuts .shortcut-keys .gesture {
-  color: #333;
-  margin: 0 3px;
-}
-
 
 /* Settings Modals (custom background, custom data)
 ------------------------------------------------------- */

modules/core/ImagerySystem.js
@@ -575,7 +575,10 @@ export class ImagerySystem extends AbstractSystem {
   set brightness(val = 1) {
     if (val === this._brightness) return;  // no change
     this._brightness = val;
-    this.context.scene().layers.get('background')?.setBrightness(val);
+
+    const context = this.context;
+    const scene = context.systems.gfx.scene;
+    scene.layers.get('background')?.setBrightness(val);
     this.emit('imagerychange');
   }
 
@@ -589,7 +592,10 @@ export class ImagerySystem extends AbstractSystem {
   set contrast(val = 1) {
     if (val === this._contrast) return;  // no change
     this._contrast = val;
-    this.context.scene().layers.get('background')?.setContrast(val);
+
+    const context = this.context;
+    const scene = context.systems.gfx.scene;
+    scene.layers.get('background')?.setContrast(val);
     this.emit('imagerychange');
   }
 
@@ -603,7 +609,10 @@ export class ImagerySystem extends AbstractSystem {
   set saturation(val = 1) {
     if (val === this._saturation) return;  // no change
     this._saturation = val;
-    this.context.scene().layers.get('background')?.setSaturation(val);
+
+    const context = this.context;
+    const scene = context.systems.gfx.scene;
+    scene.layers.get('background')?.setSaturation(val);
     this.emit('imagerychange');
   }
 
@@ -617,7 +626,10 @@ export class ImagerySystem extends AbstractSystem {
   set sharpness(val = 1) {
     if (val === this._sharpness) return;  // no change
     this._sharpness = val;
-    this.context.scene().layers.get('background')?.setSharpness(val);
+
+    const context = this.context;
+    const scene = context.systems.gfx.scene;
+    scene.layers.get('background')?.setSharpness(val);
     this.emit('imagerychange');
   }
 

modules/core/Map3dSystem.js
@@ -234,7 +234,7 @@ export class Map3dSystem extends AbstractSystem {
   _setupKeybinding() {
     const context = this.context;
     const l10n = context.systems.l10n;
-    const toggleKey = utilCmd('⌘' + l10n.t('background.3dmap.key'));
+    const toggleKey = utilCmd('⌘' + l10n.t('shortcuts.command.toggle_3dmap.key'));
 
     context.keybinding().off(toggleKey);
     context.keybinding().on(toggleKey, this.toggle);

modules/core/MapSystem.js
@@ -5,7 +5,7 @@ import {
 } from '@rapid-sdk/math';
 
 import { AbstractSystem } from './AbstractSystem.js';
-import { utilCmd, utilTotalExtent } from '../util/index.js';
+import { utilTotalExtent } from '../util/index.js';
 
 const TILESIZE = 256;
 const MIN_Z = 2;
@@ -277,37 +277,18 @@ export class MapSystem extends AbstractSystem {
    */
   _setupKeybinding() {
     const context = this.context;
-    const gfx = context.systems.gfx;
     const l10n = context.systems.l10n;
 
-    const wireframeKey = l10n.t('area_fill.wireframe.key');
-    const toggleOsmKey = utilCmd('⇧' + l10n.t('map_data.layers.osm.key'));
-    const toggleNotesKey = utilCmd('⇧' + l10n.t('map_data.layers.notes.key'));
-    const highlightEditsKey = l10n.t('map_data.highlight_edits.key');
+    const wireframeKey = l10n.t('shortcuts.command.wireframe.key');
+    const highlightEditsKey = l10n.t('shortcuts.command.highlight_edits.key');
 
-    context.keybinding().off([wireframeKey, toggleOsmKey, highlightEditsKey]);
+    context.keybinding().off([wireframeKey, highlightEditsKey]);
 
     context.keybinding()
       .on(wireframeKey, e => {
         e.preventDefault();
         e.stopPropagation();
         this.wireframeMode = !this.wireframeMode;
-      })
-      .on(toggleOsmKey, e => {
-        e.preventDefault();
-        e.stopPropagation();
-
-        // Don't disappear the OSM data while drawing - iD#6584
-        const mode = context.mode;
-        if (mode && /^draw/.test(mode.id)) return;
-
-        gfx.scene.toggleLayers('osm');
-        context.enter('browse');
-      })
-      .on(toggleNotesKey, e => {
-        e.preventDefault();
-        e.stopPropagation();
-        gfx.scene.toggleLayers('notes');
       })
       .on(highlightEditsKey, e => {
         e.preventDefault();

modules/core/PhotoSystem.js
@@ -45,7 +45,6 @@ export class PhotoSystem extends AbstractSystem {
     this._hashchange = this._hashchange.bind(this);
     this._layerchange = this._layerchange.bind(this);
     this._photoChanged = this._photoChanged.bind(this);
-    this._keydown = this._keydown.bind(this);
   }
 
 
@@ -79,62 +78,7 @@ export class PhotoSystem extends AbstractSystem {
         // Setup event handlers..
         urlhash.on('hashchange', this._hashchange);
         gfx.scene.on('layerchange', this._layerchange);
-        const wireframeKey = '⇧M';
-        const toggleOsmKey = '⇧S';
-        const toggleNotesKey = '⇧K';
-        context.keybinding()
-        .on(wireframeKey, e => {
-          e.preventDefault();
-          this.toggleLayer('mapillary');
-        })
-        .on(toggleOsmKey, e => {
-          e.preventDefault();
-          this.toggleLayer('streetside');
-        })
-        .on(toggleNotesKey, e => {
-          e.preventDefault();
-          this.toggleLayer('kartaview');
-        });
       });
-  }
-
-
-  /**
-   * _keydown
-   * Handles keydown events to toggle photo layers.
-   * @param {KeyboardEvent} e - The keyboard event.
-   */
-  _keydown(e) {
-    if (e.shiftKey && e.key === 'M') {
-      e.preventDefault();
-      this.toggleLayer('mapillary');
-    } else if (e.shiftKey && e.key === 'S') {
-      e.preventDefault();
-      this.toggleLayer('streetside');
-    } else if (e.shiftKey && e.key === 'K') {
-      e.preventDefault();
-      this.toggleLayer('kartaview');
-    }
-  }
-
-
-  /**
-   * toggleLayer
-   * Toggles the specified photo layer on or off.
-   * @param {string} layerID - The ID of the layer to toggle.
-   */
-  toggleLayer(layerID) {
-    const scene = this.context.systems.gfx.scene;
-    const layer = scene.layers.get(layerID);
-    if (layer) {
-      layer.enabled = !layer.enabled;
-      this._photoChanged();
-      if (layer.enabled) {
-        this.selectPhoto(layerID);
-      } else {
-        this.selectPhoto(); // Deselect if turned off
-      }
-    }
   }
 
 
@@ -705,7 +649,9 @@ export class PhotoSystem extends AbstractSystem {
    * @return {boolean}  `true` if enabled, `false` if not
    */
   isLayerEnabled(layerID) {
-    const layer = this.context.scene().layers.get(layerID);
+    const context = this.context;
+    const scene = context.systems.gfx.scene;
+    const layer = scene.layers.get(layerID);
     return layer?.enabled;
   }
 

modules/operations/circularize.js
@@ -108,7 +108,7 @@ export function operationCircularize(context, selectedIDs) {
 
 
   operation.id = 'circularize';
-  operation.keys = [ l10n.t('operations.circularize.key') ];
+  operation.keys = [ l10n.t('shortcuts.command.circularize.key') ];
   operation.title = l10n.t('operations.circularize.title');
   operation.behavior = new KeyOperationBehavior(context, operation);
 

commit d30bb66a2824149f6aa607364a01e273663e7dec

Fix layer checkboxes, regressed in a31bf0756

modules/ui/sections/data_layers.js
@@ -222,9 +222,9 @@ export function uiSectionDataLayers(context) {
     // Update
     li
       .merge(liEnter)
-      .classed('active', d => d.enabled)
+      .classed('active', d => showsLayer(d.id))
       .selectAll('input')
-      .property('checked', d => d.enabled);
+      .property('checked', d => showsLayer(d.id));
   }
 
 