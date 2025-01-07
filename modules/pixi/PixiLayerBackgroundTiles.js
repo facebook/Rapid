facebook/Rapid

commit 7814fbe27a52b657488e4b060132bc0cbac3a03e

Remove extra container nesting and old 'todo'
The reason for this was:
A long time ago each Layer had a `this.container`.  We moved away from this
so that we could sort things in the scene better, and switched to using
"group containers" in PixiScene to manage the sorting. The Layers themselves
still manage data but now add it to the appropriate "group container" for rendering.

MapUI is a special layer that lives above everything else.  We still had some
code around that would expect MapUI Layer to still have a main `.container`,
then the code would look for a child that it really wanted - for example

```js
const mapUIContainer = this.scene.layers.get('map-ui').container;
const selectedContainer = mapUIContainer.getChildByName('selected');
```

Now code like this is rewritten to just get what it needs instead of expecting
there to be an extra container with children.

modules/pixi/PixiLayerBackgroundTiles.js
@@ -145,8 +145,7 @@ export class PixiLayerBackgroundTiles extends AbstractLayer {
     let debugContainer;
     if (!this.isMinimap) {
       showDebug = context.getDebug('tile');
-      const mapUIContainer = this.scene.layers.get('map-ui').container;
-      debugContainer = mapUIContainer.getChildByLabel('tile-debug');
+      const debugContainer = this.scene.layers.get('map-ui').tileDebug;
       debugContainer.visible = showDebug;
     }
 
@@ -286,7 +285,7 @@ export class PixiLayerBackgroundTiles extends AbstractLayer {
             debugContainer.addChild(tile.text);
           }
 
-          tile.debug.position.set(x, y - size);          // left, top
+          tile.debug.position.set(x, y - size);         // left, top
           tile.text.position.set(x + 2, y - size + 2);  // left, top
           tile.debug
             .clear()

modules/pixi/PixiFeatureLine.js
@@ -332,8 +332,8 @@ export class PixiFeatureLine extends AbstractFeature {
       if (!this.halo) {
         this.halo = new PIXI.Graphics();
         this.halo.label = `${this.id}-halo`;
-        const mapUIContainer = this.scene.layers.get('map-ui').container;
-        mapUIContainer.addChild(this.halo);
+        const haloContainer = this.scene.layers.get('map-ui').halo;
+        haloContainer.addChild(this.halo);
       }
 
       const HALO_STYLE = {

modules/pixi/PixiFeaturePoint.js
@@ -354,8 +354,8 @@ export class PixiFeaturePoint extends AbstractFeature {
       if (!this.halo) {
         this.halo = new PIXI.Graphics();
         this.halo.label = `${this.id}-halo`;
-        const mapUIContainer = this.scene.layers.get('map-ui').container;
-        mapUIContainer.addChild(this.halo);
+        const haloContainer = this.scene.layers.get('map-ui').halo;
+        haloContainer.addChild(this.halo);
       }
 
       const HALO_STYLE = {

modules/pixi/PixiFeaturePolygon.js
@@ -460,8 +460,8 @@ export class PixiFeaturePolygon extends AbstractFeature {
       if (!this.halo) {
         this.halo = new PIXI.Graphics();
         this.halo.label = `${this.id}-halo`;
-        const mapUIContainer = this.scene.layers.get('map-ui').container;
-        mapUIContainer.addChild(this.halo);
+        const haloContainer = this.scene.layers.get('map-ui').halo;
+        haloContainer.addChild(this.halo);
       }
 
       const HALO_STYLE = {

modules/pixi/PixiLayerLabels.js
@@ -350,8 +350,7 @@ export class PixiLayerLabels extends AbstractLayer {
     // Gather the containers that have avoidable stuff on them
     const avoidContainers = [];
 
-    const mapUIContainer = this.scene.layers.get('map-ui').container;
-    const selectedContainer = mapUIContainer.getChildByLabel('selected');
+    const selectedContainer = this.scene.layers.get('map-ui').selected;
     if (selectedContainer) {
       avoidContainers.push(selectedContainer);
     }

modules/pixi/PixiLayerMapUI.js
@@ -29,15 +29,6 @@ export class PixiLayerMapUI extends AbstractLayer {
     super(scene, layerID);
     this.enabled = true;   // this layer should always be enabled
 
-// todo: I'm adjusting the container nesting, this will need to be revisited
-const container = new PIXI.Container();
-container.label= layerID;
-container.sortableChildren = true;
-this.container = container;
-
-const groupContainer = this.scene.groups.get('ui');
-groupContainer.addChild(container);
-
     this._oldk = 0;
 
     // setup the child containers
@@ -46,27 +37,34 @@ groupContainer.addChild(container);
     // GEOLOCATION
     this._geolocationData = null;
     this._geolocationDirty = false;
-    const geolocationContainer = new PIXI.Container();
-    geolocationContainer.label= 'geolocation';
-    geolocationContainer.eventMode = 'none';
-    geolocationContainer.sortableChildren = false;
-    geolocationContainer.visible = false;
-    this.geolocationContainer = geolocationContainer;
+    const geolocation = new PIXI.Container();
+    geolocation.label= 'geolocation';
+    geolocation.eventMode = 'none';
+    geolocation.sortableChildren = false;
+    geolocation.visible = false;
+    this.geolocation = geolocation;
 
     // TILE DEBUGGING
-    const tileDebugContainer = new PIXI.Container();
-    tileDebugContainer.label= 'tile-debug';
-    tileDebugContainer.eventMode = 'none';
-    tileDebugContainer.sortableChildren = false;
-    tileDebugContainer.visible = false;
-    this.tileDebugContainer = tileDebugContainer;
+    const tileDebug = new PIXI.Container();
+    tileDebug.label= 'tile-debug';
+    tileDebug.eventMode = 'none';
+    tileDebug.sortableChildren = false;
+    tileDebug.visible = false;
+    this.tileDebug = tileDebug;
 
     // SELECTED
-    const selectedContainer = new PIXI.Container();
-    selectedContainer.label= 'selected';
-    selectedContainer.sortableChildren = true;
-    selectedContainer.visible = true;
-    this.selectedContainer = selectedContainer;
+    const selected = new PIXI.Container();
+    selected.label = 'selected';
+    selected.sortableChildren = true;
+    selected.visible = true;
+    this.selected = selected;
+
+    // HALO
+    const halo = new PIXI.Container();
+    halo.label = 'halo';
+    halo.sortableChildren = true;
+    halo.visible = true;
+    this.halo = halo;
 
     // Lasso polygon
     this._lassoData = null;
@@ -77,10 +75,11 @@ groupContainer.addChild(container);
     lasso.label= 'lasso';
     lasso.eventMode = 'none';
     lasso.sortableChildren = false;
-    lasso.visible = true;
+    lasso.visible = false;
     this.lasso = lasso;
 
-    this.container.addChild(geolocationContainer, tileDebugContainer, selectedContainer, lasso);
+    const groupContainer = this.scene.groups.get('ui');
+    groupContainer.addChild(geolocation, tileDebug, selected, halo, lasso);
   }
 
 
@@ -173,6 +172,7 @@ groupContainer.addChild(container);
     const data = this._lassoData;
 
     if (Array.isArray(data) && data.length > 1) {  // should show lasso
+      container.visible = true;
       if (!container.children.length) {
         container.addChild(line, fill);
       }
@@ -197,6 +197,7 @@ groupContainer.addChild(container);
       fill.clear().poly(flatCoords).fill(fillStyle);
 
     } else {  // no lasso data
+      container.visible = false;
       if (container.children.length) {
         container.removeChildren();
       }
@@ -213,52 +214,56 @@ groupContainer.addChild(container);
    * @param  viewport   Pixi viewport to use for rendering
    */
   renderGeolocation(frame, viewport) {
-    if (this._geolocationDirty) {
-      this._geolocationDirty = false;
-      this.geolocationContainer.removeChildren();
+    if (!this._geolocationDirty) return;
 
-      if (this.geolocationData && this.geolocationData.coords) {
-        const d = this.geolocationData.coords;
-        const coord = [d.longitude, d.latitude];
-        const [x, y] = viewport.project(coord);
+    const container = this.geolocation;
 
-        // Calculate the radius of the accuracy aura (convert meters -> pixels)
-        const dLon = geoMetersToLon(d.accuracy, coord[1]);  // coord[1] = at this latitude
-        const edge = [d.longitude + dLon, d.latitude];
-        const x2 = viewport.project(edge)[0];
-        const r = Math.max(Math.abs(x2 - x), 15);
-        const BLUE = 0xe60ff;
+    container.removeChildren();
 
-        const locatorAura = new PIXI.Graphics()
-          .circle(x, y, r)
-          .fill({ color: BLUE, alpha: 0.4 });
-        locatorAura.label = 'aura';
-        this.geolocationContainer.addChild(locatorAura);
+    if (this.geolocationData && this.geolocationData.coords) {
+      container.visible = true;
 
-        // Show a viewfield for the heading if we have it
-        if (d.heading !== null && !isNaN(d.heading)) {
-          const textures = this.gfx.textures;
-          const locatorHeading = new PIXI.Sprite(textures.get('viewfieldDark'));
-          locatorHeading.anchor.set(0.5, 1);  // middle, top
-          locatorHeading.angle = d.heading;
-          locatorHeading.label = 'heading';
-          locatorHeading.position.set(x, y);
-          this.geolocationContainer.addChild(locatorHeading);
-        }
+      const d = this.geolocationData.coords;
+      const coord = [d.longitude, d.latitude];
+      const [x, y] = viewport.project(coord);
 
-        const locatorPosition = new PIXI.Graphics()
-          .circle(x, y, 6.5)
-          .stroke(1.5, 0xffffff, 1.0)
-          .fill({ color: BLUE, alpha: 1.0 });
-        locatorPosition.label = 'position';
-        this.geolocationContainer.addChild(locatorPosition);
+      // Calculate the radius of the accuracy aura (convert meters -> pixels)
+      const dLon = geoMetersToLon(d.accuracy, coord[1]);  // coord[1] = at this latitude
+      const edge = [d.longitude + dLon, d.latitude];
+      const x2 = viewport.project(edge)[0];
+      const r = Math.max(Math.abs(x2 - x), 15);
+      const BLUE = 0xe60ff;
 
-        this.geolocationContainer.visible = true;
+      const aura = new PIXI.Graphics()
+        .circle(x, y, r)
+        .fill({ color: BLUE, alpha: 0.4 });
+      aura.label = 'aura';
+      container.addChild(aura);
 
-      } else {
-        this.geolocationContainer.visible = false;
+      // Show a viewfield for the heading if we have it
+      if (d.heading !== null && !isNaN(d.heading)) {
+        const textures = this.gfx.textures;
+        const heading = new PIXI.Sprite(textures.get('viewfieldDark'));
+        heading.anchor.set(0.5, 1);  // middle, top
+        heading.angle = d.heading;
+        heading.label = 'heading';
+        heading.position.set(x, y);
+        container.addChild(heading);
       }
+
+      const position = new PIXI.Graphics()
+        .circle(x, y, 6.5)
+        .stroke(1.5, 0xffffff, 1.0)
+        .fill({ color: BLUE, alpha: 1.0 });
+      position.label = 'position';
+      container.addChild(position);
+
+    } else {
+      container.visible = false;
     }
+
+    this._geolocationDirty = false;
+
   }
 
 }

modules/pixi/PixiLayerOsm.js
@@ -544,8 +544,7 @@ export class PixiLayerOsm extends AbstractLayer {
     const presets = context.systems.presets;
 
     // Vertices related to the selection/hover should be drawn above everything
-    const mapUIContainer = this.scene.layers.get('map-ui').container;
-    const selectedContainer = mapUIContainer.getChildByLabel('selected');
+    const selectedContainer = this.scene.layers.get('map-ui').selected;
     const pointsContainer = this.scene.groups.get('points');
 
     function isInterestingVertex(node) {
@@ -740,8 +739,7 @@ export class PixiLayerOsm extends AbstractLayer {
     const entities = new Map([...data.lines, ...data.polygons]);
 
     // Midpoints should be drawn above everything
-    const mapUIContainer = this.scene.layers.get('map-ui').container;
-    const selectedContainer = mapUIContainer.getChildByLabel('selected');
+    const selectedContainer = this.scene.layers.get('map-ui').selected;
 
     // Generate midpoints from all the highlighted ways
     let midpoints = new Map();

commit af7501d9c46e8eee86ac243f35c9bd37611059fd

Restore tile debugging display (closes #1666)
This regressed in 7814fbe when I accidently added an extra debugContainer variable.

modules/pixi/PixiLayerBackgroundTiles.js
@@ -145,7 +145,7 @@ export class PixiLayerBackgroundTiles extends AbstractLayer {
     let debugContainer;
     if (!this.isMinimap) {
       showDebug = context.getDebug('tile');
-      const debugContainer = this.scene.layers.get('map-ui').tileDebug;
+      debugContainer = this.scene.layers.get('map-ui').tileDebug;
       debugContainer.visible = showDebug;
     }
 