ef3d9b0404c3699c095a596b0af0801a0a06e49c
Tobias <t@tobiasjordans.de>
2025-02-07 11:24:20 +0100

    maproulette_details: Remove wrapper

--- modules/ui/maproulette_details.js
+++ modules/ui/maproulette_details.js
@@ -142,7 +142,6 @@ export function uiMapRouletteDetails(context) {
           .append('h4')
           .text(l10n.t('map_data.layers.maproulette.detail_title'));
         selection
-          .append('div')
           .html(description)  // parsed markdown
           .selectAll('a')
           .attr('rel', 'noopener')
@@ -154,7 +153,6 @@ export function uiMapRouletteDetails(context) {
           .append('h4')
           .text(l10n.t('map_data.layers.maproulette.instruction_title'));
         selection
-          .append('div')
           .html(instruction)  // parsed markdown
           .selectAll('a')
           .attr('rel', 'noopener')
